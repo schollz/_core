180743f5eb561d895abe66e6592b0a6eb08fd353
Zack Scholl <zack.scholl@gmail.com>
2023-08-21 19:00:30 +0200

    smalls
    Former-commit-id: f816261caba404dfbd2a502346c2b6d662c7de1e

--- lib/test/envelope3/main.c
+++ lib/test/envelope3/main.c
@@ -35,7 +35,7 @@ int main() {
   uint32_t sampleRate = 44100;
 
   Envelope3 *envelope3 =
-      Envelope3_create(sampleRate, 0.1, 1.1, 1.0, 0.0, 0.5, 0.6, 0.7);
+      Envelope3_create(sampleRate, 0.1, 1.1, 1.1, 0.0, 0.5, 0.6, 0.9);
   for (int i = 0; i < 88200; i++) {
     float value = Envelope3_update(envelope3);
     printf("%2.3f\n", value);
