a82863b37ad51992ba8b5f978555ec665b6529f6
Zack Scholl <zack.scholl@gmail.com>
2023-08-21 18:56:19 +0200

    add another envelope
    Former-commit-id: 83b083427a54b8201f27e94b66b7330dfe89208d

---
+++ lib/test/envelope3/main.c
@@ -0,0 +1,23 @@
+//  gcc -o main main.c && ./main  | gnuplot -p -e 'plot "/dev/stdin"  using 0:1'
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../envelope3.h"
+
+int main() {
+  // Create a Envelope3 instance
+  uint32_t sampleRate = 44100;
+
+  Envelope3 *envelope3 =
+      Envelope3_create(sampleRate, 0.1, 1.1, 1.0, 0.0, 0.5, 0.6, 0.7);
+  for (int i = 0; i < 88200; i++) {
+    float value = Envelope3_update(envelope3);
+    printf("%2.3f\n", value);
+  }
+
+  Envelope3_destroy(envelope3);
+
+  return 0;
+}
