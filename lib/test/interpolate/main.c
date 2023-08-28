13e051f4534cf322a127d715b4940508bca39ff3
Zack Scholl <zack.scholl@gmail.com>
2023-08-28 17:35:16 +0000

    add more interpolations
    Former-commit-id: 6e51aada6e9ddd089c1f7a5f5f21fc993a85484e

--- lib/test/interpolate/main.c
+++ lib/test/interpolate/main.c
@@ -36,7 +36,7 @@
 int main() {
   int16_t arr[] = {11620, 10309, 6301, 2171, 650, 2136, 4150, 4507};
   int16_t arr_size = sizeof(arr) / sizeof(arr[0]);
-  int16_t arr_new_size = 4;
+  int16_t arr_new_size = 80;
   int16_t *newArray = array_resample_linear(arr, arr_size, arr_new_size);
   for (int i = 0; i < arr_new_size; i++) {
     printf("%f,%d\n", (float)i / (float)(arr_new_size - 1), newArray[i]);
@@ -44,7 +44,16 @@ int main() {
   for (int i = 0; i < arr_size; i++) {
     printf("%f,%d\n", (float)i / (float)(arr_size - 1), arr[i]);
   }
+  newArray = array_resample_linear2(arr, arr_size, arr_new_size);
+  for (int i = 0; i < arr_new_size; i++) {
+    printf("%f,%d\n", (float)i / (float)(arr_new_size - 1), newArray[i]);
+  }
+  newArray = array_resample_quadratic(arr, arr_size, arr_new_size);
+  for (int i = 0; i < arr_new_size; i++) {
+    printf("%f,%d\n", (float)i / (float)(arr_new_size - 1), newArray[i]);
+  }
 
   free(newArray);
+
   return 0;
 }
