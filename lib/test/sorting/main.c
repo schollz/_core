b87bbbe98ee8b881ad89f564b07d48854a1d012a
Zack Scholl <zack.scholl@gmail.com>
2023-09-08 07:45:51 -0700

    add sort
    Former-commit-id: 99bd03980a35ba591f98532fc2ccfb770040d9f9

--- lib/test/sorting/main.c
+++ lib/test/sorting/main.c
@@ -2,27 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-uint16_t *sort_int16_t(int16_t array[], int n) {
-  // Create an auxiliary array to store the indexes.
-  uint16_t *indexes = malloc(sizeof(uint16_t) * n);
-  for (int i = 0; i < n; i++) {
-    indexes[i] = i;
-  }
-
-  // Sort the auxiliary array based on the values in the original array.
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (array[indexes[i]] > array[indexes[j]]) {
-        // Swap the elements at indexes[i] and indexes[j].
-        int temp = indexes[i];
-        indexes[i] = indexes[j];
-        indexes[j] = temp;
-      }
-    }
-  }
-
-  return indexes;
-}
+#include "../../sort.h"
 
 int main() {
   int16_t array[] = {10, 20, 30, 40, 1, -1, -1};
