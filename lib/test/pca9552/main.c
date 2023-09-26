cf94235b32e4f8af418b4f87a0044de6196283c4
Zack Scholl <zack.scholl@gmail.com>
2023-09-26 07:56:10 -0700

    working
    Former-commit-id: 2e78cab5e8006c6be34ba3aee354f8fa4c8227e5

--- lib/test/pca9552/main.c
+++ lib/test/pca9552/main.c
@@ -15,39 +15,30 @@ int main(void) {
   stdio_init_all();
   sleep_ms(1000);
 
-  i2c_init(i2c_default, 50 * 1000);
+  i2c_init(i2c_default, 400 * 1000);
   gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
   gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
   gpio_pull_up(I2C_SDA_PIN);
   gpio_pull_up(I2C_SCL_PIN);
-
-  while (true) {
-    sleep_ms(1000);
-  }
 
   PCA9552 *pca;
   pca = PCA9552_create(0x60, i2c_default);
   if (pca->error != PCA9552_OK) {
     printf("PCA9552_ERROR: %02x\n", pca->error);
   }
-  sleep_ms(1000);
-  free(pca);
 
   while (1) {
-    for (uint8_t i = 0; i < 16; i++) {
-      PCA9552_digitalWrite(pca, i, 1);
-      if (pca->error != PCA9552_OK) {
-        printf("PCA9552_ERROR: %02x\n", pca->error);
-      }
-    }
     sleep_ms(1000);
-    for (uint8_t i = 0; i < 16; i++) {
-      PCA9552_digitalWrite(pca, i, 0);
-      if (pca->error != PCA9552_OK) {
-        printf("PCA9552_ERROR: %02x\n", pca->error);
-      }
-    }
+    PCA9552_writeReg(pca, 0x06, 0x00);
+    PCA9552_writeReg(pca, 0x07, 0x55);
+    PCA9552_writeReg(pca, 0x08, 0x00);
+    PCA9552_writeReg(pca, 0x09, 0x6C);
     sleep_ms(1000);
+    PCA9552_writeReg(pca, 0x06, 0x55);
+    PCA9552_writeReg(pca, 0x07, 0x00);
+    PCA9552_writeReg(pca, 0x08, 0x6C);
+    PCA9552_writeReg(pca, 0x09, 0x00);
   }
+  free(pca);
   return 0;
 }
