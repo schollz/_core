da5b3c536763afa0f07c3db463036f60b70691d6
Zack Scholl <zack.scholl@gmail.com>
2023-10-02 07:04:42 -0700

    refactor
    Former-commit-id: 339260c8bdcf462bcaacba6c3ecd4df4528f276f

---
+++ lib/includes.h
@@ -0,0 +1,66 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "hardware/adc.h"
+#include "hardware/clocks.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/pll.h"
+#include "hardware/rtc.h"
+#include "hardware/structs/clocks.h"
+#include "pico/audio_i2s.h"
+#include "pico/multicore.h"
+#include "pico/stdlib.h"
+#include "pico/types.h"
+//
+#include "ff.h" /* Obtains integer types */
+//
+#include "diskio.h" /* Declarations of disk functions */
+//
+#include "f_util.h"
+#include "hw_config.h"
+#include "my_debug.h"
+#include "sd_card.h"
+//
+#include "definitions.h"
+#include "pcg_basic.h"
+//
+#include "array_resample.h"
+#include "audio_pool.h"
+#ifdef INCLUDE_BASS
+#include "bass.h"
+#endif
+#ifdef INCLUDE_RGBLED
+#include "WS2812.h"
+#endif
+#include "biquad.h"
+#include "buttonmatrix2.h"
+#include "charlieplex.h"
+#include "crossfade.h"
+#include "envelope2.h"
+#include "envelopegate.h"
+#include "file_list.h"
+#include "iir.h"
+#include "noise.h"
+#include "pca9552.h"
+#include "random.h"
+#include "savefile.h"
+#include "sdcard.h"
+#include "selectx.h"
+#include "wav.h"
+//
+#include "transfer_distortion.h"
+#include "transfer_saturate.h"
+//
+#include "transfer.h"
+//
+#include "globals.h"
+//
+#include "sdcard_startup.h"
+//
+#include "keyboard.h"
---
+++ lib/keyboard.h
@@ -0,0 +1,208 @@
+void run_keyboard() {
+  int c = getchar_timeout_us(0);
+  if (c >= 0) {
+    if (c == '-' && sf->vol) sf->vol--;
+    if ((c == '=' || c == '+') && sf->vol < 256) sf->vol++;
+    if (c == ']') {
+      if (sf->bpm_tempo < 300) {
+        sf->bpm_tempo += 5;
+      }
+      printf("\nbpm: %d\n\n", sf->bpm_tempo);
+    }
+    if (c == 'c') {
+      SaveFile_PatternRandom(sf, &rng, 0, 3);
+      SaveFile_PatternPrint(sf);
+      sf->pattern_on = 1 - sf->pattern_on;
+    }
+    if (c == 'v') {
+      printf("current beat: %d, phase_new: %d, cpu util: %d\n", beat_current,
+             phase_new, cpu_utilization);
+#ifdef INCLUDE_BASS
+      Bass_trig(bass, 0);
+#endif
+    }
+    if (c == '[') {
+      if (sf->bpm_tempo > 30) {
+        sf->bpm_tempo -= 5;
+      }
+      printf("\nbpm: %d\n\n", sf->bpm_tempo);
+    }
+    if (c == 'p') {
+      phase_forward = !phase_forward;
+    }
+    if (c == 'w') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 0 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'e') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 1 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'r') {
+      // phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 2
+      // / 16; phase_new = (phase_new / 4) * 4; phase_change = true;
+      // debounce_quantize = 2;
+      retrig_first = true;
+      retrig_beat_num = random_integer_in_range(8, 24);
+      retrig_timer_reset =
+          96 * random_integer_in_range(1, 4) / random_integer_in_range(2, 12);
+      float total_time = (float)(retrig_beat_num * retrig_timer_reset * 60) /
+                         (float)(96 * sf->bpm_tempo);
+      retrig_vol_step = 1.0 / ((float)retrig_beat_num);
+      printf("retrig_beat_num=%d,retrig_timer_reset=%d,total_time=%2.3f s\n",
+             retrig_beat_num, retrig_timer_reset, total_time);
+      retrig_ready = true;
+    }
+    if (c == 't') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 3 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'y') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 4 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'u') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 5 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'i') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 6 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'i') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 7 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 's') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 8 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'd') {
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 9 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+      debounce_quantize = 2;
+    }
+    if (c == 'f') {
+      debounce_quantize = 2;
+      beat_current = 10;
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 10 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+    }
+    if (c == 'g') {
+      debounce_quantize = 2;
+      beat_current = 11;
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 11 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+    }
+    if (c == 'h') {
+      debounce_quantize = 2;
+      beat_current = 12;
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 12 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+    }
+    if (c == 'j') {
+      debounce_quantize = 2;
+      beat_current = 13;
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 13 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+    }
+    if (c == 'k') {
+      debounce_quantize = 2;
+      beat_current = 14;
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 14 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+    }
+    if (c == 'l') {
+      debounce_quantize = 2;
+      beat_current = 15;
+      phase_new = (file_list[fil_current_bank].size[fil_current_id]) * 15 / 16;
+      phase_new = (phase_new / 4) * 4;
+      phase_change = true;
+    }
+    if (c == 'b') {
+      run_mount();
+    }
+    if (c == 'n') {
+      // big_file_test("testfile", 1, 1);
+      SaveFile_Load(sf);
+    }
+    if (c == 'm') {
+      SaveFile_Save(sf, &sync_using_sdcard);
+    }
+    if (c == '9') {
+      sf->saturate_wet++;
+      printf("saturate_wet: %d\n", sf->saturate_wet);
+    }
+    if (c == '8') {
+      if (sf->saturate_wet > 0) {
+        sf->saturate_wet--;
+      }
+      printf("saturate_wet: %d\n", sf->saturate_wet);
+    }
+    if (c == '.') {
+      sf->distortion_level++;
+      printf("distortion_level: %d\n", sf->distortion_level);
+    }
+    if (c == ',') {
+      if (sf->distortion_level > 0) {
+        sf->distortion_level--;
+      }
+      printf("distortion_level: %d\n", sf->distortion_level);
+    }
+    if (c == '1') {
+      fil_current_id_next = 0;
+      fil_current_change = true;
+    }
+    if (c == '2') {
+      fil_current_id_next = 1;
+      fil_current_change = true;
+    }
+    if (c == '3') {
+      fil_current_id_next = 2;
+      fil_current_change = true;
+    }
+    if (c == 'a') {
+      Envelope2_reset(envelope3, BLOCKS_PER_SECOND, 0, 1.0, 1.5);
+      // debounce_quantize = 2;
+    }
+    if (c == 'q') {
+      Envelope2_reset(envelope_pitch, BLOCKS_PER_SECOND,
+                      Envelope2_update(envelope_pitch), 1.0, 1);
+      debounce_quantize = 2;
+    }
+    if (c == 'z') {
+      debounce_quantize = 2;
+      Envelope2_reset(envelope_pitch, BLOCKS_PER_SECOND,
+                      Envelope2_update(envelope_pitch), 0.5, 1);
+    }
+    if (c == 'x') {
+      sdcard_startup();
+      // for (uint16_t i = 0; i < 2202; i++) {
+      //   printf("%d\n", sf->vol - crossfade_vol(sf->vol, i));
+      // }
+    }
+    // printf("sf->vol = %d      \r", sf->vol);
+  }
+}
