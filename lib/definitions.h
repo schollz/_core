339260c8bdcf462bcaacba6c3ecd4df4528f276f
Zack Scholl <zack.scholl@gmail.com>
2023-10-02 07:04:42 -0700

    refactor

---
+++ lib/definitions.h
@@ -0,0 +1,24 @@
+#define debugf(format, ...) \
+  printf(format " %s:%d\n", __VA_ARGS__, __FILE__, __LINE__)
+
+// sample rate is defined by the codec, PCM5102
+// blocks per second is defined by SAMPLES_PER_BUFFER
+// which can be modified
+#ifdef INCLUDE_STEREO
+#define WAV_CHANNELS 2
+#endif
+#ifndef INCLUDE_STEREO
+#define WAV_CHANNELS 1
+#endif
+
+#define MAX_VOLUME 100
+#define BLOCKS_PER_SECOND SAMPLE_RATE / SAMPLES_PER_BUFFER
+static int PHASE_DIVISOR = WAV_CHANNELS * 2;
+
+// keys
+#define KEY_SHIFT 0
+#define KEY_A 1
+#define KEY_B 2
+#define KEY_C 3
+
+static const uint32_t PIN_DCDC_PSM_CTRL = 23;
