c239b9b139217003e93c49b9d77a81c8f3aa2c78
Zack Scholl <zack.scholl@gmail.com>
2023-10-06 18:41:57 -0700

    show X if not loaded correctly

--- lib/sdcard_startup.h
+++ lib/sdcard_startup.h
@@ -22,13 +22,10 @@ void sdcard_startup() {
   bass = Bass_create();
 #endif
 
-  printf("\nz!!\n");
-
   for (uint8_t i = 0; i < 1; i++) {
     char dirname[10];
     sprintf(dirname, "bank%d\0", i);
     file_list[i] = list_files(dirname, WAV_CHANNELS);
-    printf("bank %d, ", i);
     printf("found %d files\n", file_list[i].num);
     printf("file_list[i].name[0]: %s\n", file_list[i].name[0]);
     printf("file_list[i].bpm[0]: %d\n", file_list[i].bpm[0]);
