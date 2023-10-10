f90c14b3699feb814c3210c3d307e26e50f91395
Zack Scholl <zack.scholl@gmail.com>
2023-10-10 11:55:46 -0700

    add all
    Former-commit-id: da3d6b909bf3745bd7d7d2c3f41a858cd3e20c7d

--- lib/test/onewiremidipio/onewiremidi.c
+++ lib/test/onewiremidipio/onewiremidi.c
@@ -1,3 +1,4 @@
+
 #include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
@@ -8,8 +9,9 @@
 
 #include "hardware/clocks.h"
 #include "hardware/pio.h"
-#include "onewiremidi.pio.h"
 #include "pico/stdlib.h"
+//
+#include "onewiremidi.h"
 
 void printBinaryRepresentation(uint8_t num) {
   // Iterate through each bit position (from 31 to 0)
@@ -28,43 +30,24 @@ uint8_t reverse_uint8_t(uint8_t b) {
   return b;
 }
 
+void midi_note_on(uint8_t note, uint8_t vel) {
+  printf("note on\t%d\t%d", note, vel);
+}
+
+void midi_note_off(uint8_t note) { printf("note off\t%d", note); }
+
 int main() {
   stdio_init_all();
 
   sleep_ms(1500);  // Wait for the circuit to settle
   printf("clock freq: %2.3f\n", (float)clock_get_hz(clk_sys));
 
-  const uint PIN_CAPTURE = 18;  // Replace with your desired GPIO pin
-  PIO pio = pio0;
-  unsigned char sm = 0;
-  uint offset = pio_add_program(pio0, &midi_rx_program);
-  pio_sm_config c = midi_rx_program_get_default_config(offset);
-  sm_config_set_in_pins(&c, PIN_CAPTURE);
-  pio_sm_set_consecutive_pindirs(pio, sm, PIN_CAPTURE, 1, true);
-  sm_config_set_set_pins(&c, PIN_CAPTURE, 1);
-  sm_config_set_in_shift(&c, 0, 0, 0);  // Corrected the shift setup
-  pio_sm_init(pio, sm, offset, &c);
-  pio_sm_set_clkdiv(pio, sm,
-                    (float)clock_get_hz(clk_sys) / 1000000.0f);  // 1 us/cycle
-  pio_sm_set_enabled(pio, sm, true);
+  Onewiremidi *om;
+  om = Onewiremidi_new(pio0, 0, 18, midi_note_on, midi_note_off, NULL, NULL,
+                       NULL, NULL);
 
-  uint8_t rbs[3];
-  uint8_t rbi = 0;
   while (true) {
-    if (!pio_sm_is_rx_fifo_empty(pio, sm)) {
-      uint8_t value = reverse_uint8_t(pio_sm_get(pio, sm));
-      rbs[rbi] = value;
-      if (value == 0xF8 || value == 0xFE) {
-        continue;
-      }
-      rbi++;
-      if (rbi == 1 && rbs[0] == 0 && (value < 0x80 || value > 0x99)) {
-        rbi = 0;
-      } else if (rbi == 3) {
-        printf("%02X %02X %02X\n", rbs[0], rbs[1], rbs[2]);
-        rbi = 0;
-      }
-    }
+    Onewiremidi_receive(om);
   }
 
   return 0;
