b7c81d61f2c730e97fbf8a875d0f669dd10ee424
Zack Scholl <zack.scholl@gmail.com>
2023-10-13 07:58:38 -0700

    go<->c tests

---
+++ lib/test/filestruct/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+func main() {
+	buf := new(bytes.Buffer)
+	var data = []any{
+		uint32(61374), // a
+		uint32(54),    // b
+		uint8(254),    // c
+		int8(-12),     // d
+	}
+	data = append(data, int8(7))
+	data = append(data, []int8{32, 33, 34, 35, 36, 37, 38})
+	for _, v := range data {
+		err := binary.Write(buf, binary.LittleEndian, v)
+		if err != nil {
+			fmt.Println("binary.Write failed:", err)
+		}
+	}
+	fmt.Printf("%d bytes: %x", len(buf.Bytes()), buf.Bytes())
+	bufSize := new(bytes.Buffer)
+	binary.Write(bufSize, binary.LittleEndian, uint32(len(buf.Bytes())))
+
+	f, _ := os.Create("test")
+	f.Write(bufSize.Bytes())
+	f.Write(buf.Bytes())
+	f.Close()
+}
