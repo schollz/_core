d7ac1de9e37ece5c5803e87d00df0c97abac377c
Zack Scholl <zack.scholl@gmail.com>
2023-10-22 14:47:27 -0700

    add crossfader
    Former-commit-id: 1d5ce2921cb0d6fc19f5259937fb1c7d1e529aff

---
+++ lib/crossfade3.py
@@ -0,0 +1,143 @@
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+Q16_16_Q_BITS = 16
+Q16_16_FRACTIONAL_BITS = 1 << Q16_16_Q_BITS
+
+def q16_16_fp_to_int(fixedValue):
+  """Converts a Q16.16 fixed-point value to an int."""
+  return int(fixedValue >> Q16_16_Q_BITS)
+
+def q16_16_fp_to_float(value):
+  """Converts a Q16.16 fixed-point value to a float."""
+  return float(value) / Q16_16_FRACTIONAL_BITS
+
+def q16_16_float_to_fp(value):
+  """Converts a float to a Q16.16 fixed-point value."""
+  return int(value * Q16_16_FRACTIONAL_BITS)
+
+def q16_16_int_to_fp(value):
+  """Converts an int to a Q16.16 fixed-point value."""
+  return int(value) << Q16_16_Q_BITS
+
+def q16_16_multiply(a, b):
+  """Multiplies two Q16.16 fixed-point values."""
+  return int(((int64(a) * b) >> Q16_16_Q_BITS))
+
+
+
+
+
+def run():
+    block_size = int(sys.argv[1])
+    if block_size==None:
+        print("oops")
+        return
+    # Create a numpy array of x-values from 0 to 1
+    x = np.linspace(0, 1, block_size)
+
+    # Calculate the sine of the x-values
+    y = ((1 - np.sin(4 * np.pi * (x / 4) + np.pi / 2 + np.pi)) / 2)
+    y2 = np.exp(-5.4 * x)
+    y3 = 1-np.log(1.7*x+1)
+
+    print("""#ifndef CROSSFADE3_LIB
+#define CROSSFADE3_SINE 0      
+#define CROSSFADE3_EXP 1      
+#define CROSSFADE3_LOG 2      
+""")
+    print(f"static int32_t crossfade3_sine[{block_size}]={{")
+    s=""
+    for _, v in enumerate(y):
+       s+=f"{q16_16_float_to_fp(v)},"
+    print(s)
+    print("};")
+
+    print(f"static int32_t crossfade3_exp[{block_size}]={{")
+    s=""
+    for _, v in enumerate(y2):
+       s+=f"{q16_16_float_to_fp(v)},"
+    print(s)
+    print("};")
+
+    print(f"static int32_t crossfade3_log[{block_size}]={{")
+    s=""
+    for _, v in enumerate(y3):
+       s+=f"{q16_16_float_to_fp(v)},"
+    print(s)
+    print("};")
+
+    print("""
+          
+int16_t crossfade3_in(int16_t val, uint16_t i, uint8_t crossfade_type) {
+          if (crossfade_type==CROSSFADE3_SINE) {
+            return q16_16_fp_to_int16(
+          q16_16_multiply(
+          q16_16_int16_to_fp(val),
+          crossfade3_sine[i]
+          )
+            );
+          } else if (crossfade_type==CROSSFADE3_EXP) {
+            return q16_16_fp_to_int16(
+          q16_16_multiply(
+          q16_16_int16_to_fp(val),
+          crossfade3_exp[i]
+          )
+            );
+          } else {
+            return q16_16_fp_to_int16(
+          q16_16_multiply(
+          q16_16_int16_to_fp(val),
+          crossfade3_log[i]
+          )
+            );          
+          }
+}          
+int16_t crossfade3_out(int16_t val, uint16_t i, uint8_t crossfade_type) {
+          if (crossfade_type==CROSSFADE3_SINE) {
+            return q16_16_fp_to_int16(
+          q16_16_multiply(
+          q16_16_int16_to_fp(val),
+          65536-crossfade3_sine[i]
+          )
+            );
+          } else if (crossfade_type==CROSSFADE3_EXP) {
+            return q16_16_fp_to_int16(
+          q16_16_multiply(
+          q16_16_int16_to_fp(val),
+          65536-crossfade3_exp[i]
+          )
+            );
+          } else {
+            return q16_16_fp_to_int16(
+          q16_16_multiply(
+          q16_16_int16_to_fp(val),
+          65536-crossfade3_log[i]
+          )
+            );          
+          }
+}
+#define CROSSFADE3_LIB 1
+#endif
+""")
+
+    # Plot the sine wave
+    # plt.plot(x, y, label='Sine')
+    # plt.plot(x, y2, label='Exp')
+    # plt.plot(x, y3, label='Log')
+    # plt.plot(x, 1-y, label='Sine')
+    # plt.plot(x, 1-y2, label='Exp')
+    # plt.plot(x, 1-y3, label='Log')
+    # plt.legend()
+    # plt.xlabel('X')
+    # plt.ylabel('Y')
+    # plt.title('Curves')
+    # plt.show()
+
+
+
+
+if __name__ == "__main__":
+  run()
--- lib/fixedpoint.h
+++ lib/fixedpoint.h
@@ -1,23 +1,65 @@
+// Copyright 2023 Zack Scholl.
+//
+// Author: Zack Scholl (zack.scholl@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// See http://creativecommons.org/licenses/MIT/ for more information.
 
+/* Defines the number of bits used in the Q16.16 fixed-point format. */
 #define Q16_16_Q_BITS 16
+
+/* Defines the number of fractional bits used in the Q16.16 fixed-point format.
+ */
 #define Q16_16_FRACTIONAL_BITS (1 << Q16_16_Q_BITS)
 
+/* Converts a Q16.16 fixed-point value to an int16. */
 int16_t q16_16_fp_to_int16(int32_t fixedValue) {
+  /* Shift the fixed-point value right by the number of Q-bits to obtain the
+     integral part of the value. */
   return (int16_t)(fixedValue >> Q16_16_Q_BITS);
 }
 
+/* Converts a Q16.16 fixed-point value to a float. */
 float q16_16_fp_to_float(int32_t value) {
+  /* Divide the fixed-point value by the number of fractional bits to obtain
+     the floating-point value. */
   return (float)value / Q16_16_FRACTIONAL_BITS;
 }
 
+/* Converts a float to a Q16.16 fixed-point value. */
 int32_t q16_16_float_to_fp(float value) {
+  /* Multiply the floating-point value by the number of fractional bits to
+     obtain the fixed-point value. */
   return (int32_t)(value * Q16_16_FRACTIONAL_BITS);
 }
 
+/* Converts an int16 to a Q16.16 fixed-point value. */
 int32_t q16_16_int16_to_fp(int16_t value) {
+  /* Shift the int16 value left by the number of Q-bits to obtain the
+     fixed-point value. */
   return (int32_t)value << Q16_16_Q_BITS;
 }
 
+/* Multiplies two Q16.16 fixed-point values. */
 int32_t q16_16_multiply(int32_t a, int32_t b) {
+  /* Multiply the two fixed-point values and shift the result right by the
+     number of Q-bits to obtain the product. */
   return (int32_t)(((int64_t)a * b) >> Q16_16_Q_BITS);
 }
