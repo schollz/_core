bdfc0096926bd03102a1939e50f14062cdc81b30
Zack Scholl <zack.scholl@gmail.com>
2023-10-28 07:45:28 -0700

    feature: start to add sequencer
    Former-commit-id: 5021c33e7fb39f92e2b2e631a17698b349f7077f

--- lib/chain.h
+++ lib/chain.h
@@ -36,22 +36,18 @@ typedef struct Chain {
   // recording the links
   uint8_t rec_cur;
   uint8_t rec_seq[CHAIN_MAX_LINKS];
-  uint8_t rec_len;
+  uint8_t rec_last;
 } Chain;
 
 void *Chain_reset(Chain *c) { c->rec_cur = 0; }
-
-void *Chain_clear(Chain *c) {
-  c->rec_cur = 0;
-  c->rec_len = 0;
-}
 
 Chain *Chain_create() {
   Chain *c = (Chain *)malloc(sizeof(Chain));
   for (uint8_t i = 0; i < CHAIN_MAX_SEQUENCES; i++) {
     c->seq[i] = Sequencer_create();
   }
-  Chain_clear(c);
+  c->rec_cur = 0;
+  c->rec_last = 0;
   return c;
 }
 
@@ -59,24 +55,37 @@ void Chain_link(Chain *c, uint8_t *seq, uint16_t len) {
   if (len > CHAIN_MAX_LINKS) {
     len = CHAIN_MAX_LINKS;
   }
+  if (len == 0) {
+    return;
+  }
   printf("creating link of size %d:\n", len);
   for (uint8_t i = 0; i < len; i++) {
     c->rec_seq[i] = seq[i];
     printf("%d: %d\n", i, seq[i]);
   }
-  c->rec_len = len;
+  c->rec_last = len - 1;
 }
 
 void Chain_add(Chain *c, uint8_t seq, uint8_t key, uint32_t step) {
   Sequencer_add(c->seq[seq], key, step);
 }
 
+void Chain_add_current(Chain *c, uint8_t key, uint32_t step) {
+  printf("[chain] add key %d at step %d to sequence %d\n", key, step,
+         c->rec_seq[c->rec_last]);
+  Sequencer_add(c->seq[c->rec_seq[c->rec_last]], key, step);
+}
+
+void Chain_clear_seq_current(Chain *c, uint8_t i) {
+  Sequencer_clear(c->seq[c->rec_seq[c->rec_last]]);
+}
+
 int8_t Chain_emit(Chain *c, uint32_t step) {
   int8_t beat = Sequencer_emit(c->seq[c->rec_seq[c->rec_cur]], step);
   if (beat == -1 && Sequencer_is_finished(c->seq[c->rec_seq[c->rec_cur]])) {
-    if (c->rec_len > 0) {
+    if (c->rec_last > 0) {
       c->rec_cur++;
-      if (c->rec_cur > c->rec_len) {
+      if (c->rec_cur > c->rec_last) {
         c->rec_cur = 0;
       }
     }
