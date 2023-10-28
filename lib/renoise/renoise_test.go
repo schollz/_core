1e3308421c01446a3ad6e45c5eb0816800296f59
Zack Scholl <zack.scholl@gmail.com>
2023-10-28 16:43:43 -0700

    got markers
    Former-commit-id: 5fdd95fe1b75b3c360a944208ccfe4dc5c8b4f78

--- lib/renoise/renoise_test.go
+++ lib/renoise/renoise_test.go
@@ -8,11 +8,12 @@ import (
 
 func TestUnzip(t *testing.T) {
 	DebugMode = true
-	filename, markers, err := GetSliceMarkers("amen.xrni")
+	filename, start, end, err := GetSliceMarkers("amen.xrni")
 	assert.Nil(t, err)
 	assert.Equal(t, "Sample00 (Amen Brother (Version 1) 1).flac", filename)
-	assert.Equal(t, []int{0, 9403, 19561, 29757, 34833, 39376, 44021, 48488, 53694, 57783, 68060, 72827, 77613, 87215, 97040, 107191, 111672, 116746, 120879, 125783, 131073, 135262, 145403, 150270}, markers)
+	assert.Equal(t, []int{0, 9403, 19561, 29757, 34833, 39376, 44021, 48488, 53694, 57783, 68060, 72827, 77613, 87215, 97040, 107191, 111672, 116746, 120879, 125783, 131073, 135262, 145403}, start)
+	assert.Equal(t, []int{9403, 19561, 29757, 34833, 39376, 44021, 48488, 53694, 57783, 68060, 72827, 77613, 87215, 97040, 107191, 111672, 116746, 120879, 125783, 131073, 135262, 145403, 150270}, end)
 
-	_, _, err = GetSliceMarkers("does not exist.xrni")
+	_, _, _, err = GetSliceMarkers("does not exist.xrni")
 	assert.NotNil(t, err)
 }
