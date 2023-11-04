45bb8975df695ee6f2a5b4edc48df7062847842c
Zack Scholl <zack.scholl@gmail.com>
2023-11-04 10:58:06 -0700

    cos crossfade
    Former-commit-id: be328e83ee64ec7eefa6ad130d3f318141109454

--- lib/audio_callback.h
+++ lib/audio_callback.h
@@ -270,22 +270,22 @@ void i2s_callback_func() {
         uint vol = vol_main;
         if (do_crossfade) {
           if (head == 0 && !do_fade_out) {
-            newArray[i] = crossfade3_in(newArray[i], i, CROSSFADE3_LINE);
+            newArray[i] = crossfade3_in(newArray[i], i, CROSSFADE3_COS);
           } else if (!do_fade_in) {
-            newArray[i] = crossfade3_out(newArray[i], i, CROSSFADE3_LINE);
+            newArray[i] = crossfade3_out(newArray[i], i, CROSSFADE3_COS);
           }
         } else if (do_fade_out) {
-          newArray[i] = crossfade3_out(newArray[i], i, CROSSFADE3_LINE);
+          newArray[i] = crossfade3_out(newArray[i], i, CROSSFADE3_COS);
         } else if (do_fade_in) {
-          newArray[i] = crossfade3_in(newArray[i], i, CROSSFADE3_LINE);
+          newArray[i] = crossfade3_in(newArray[i], i, CROSSFADE3_COS);
         }
 
         if (do_gate_down) {
           // mute the audio
-          newArray[i] = crossfade3_out(newArray[i], i, CROSSFADE3_LINE);
+          newArray[i] = crossfade3_out(newArray[i], i, CROSSFADE3_COS);
         } else if (do_gate_up) {
           // bring back the audio
-          newArray[i] = crossfade3_in(newArray[i], i, CROSSFADE3_LINE);
+          newArray[i] = crossfade3_in(newArray[i], i, CROSSFADE3_COS);
         }
 
         if (first_loop) {
