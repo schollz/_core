2bfdf695994979dfb9abb75f173189410fae0d38
Zack Scholl <zack.scholl@gmail.com>
2023-11-04 10:57:35 -0700

    fix test
    Former-commit-id: 3756d08f8f13f6e459babf4b364b692a371ec758

--- lib/test/crossfade3/main.c
+++ lib/test/crossfade3/main.c
@@ -25,11 +25,13 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "../../fixedpoint.h"
+//
 #include "../../crossfade3.h"
 
 int main() {
   for (uint16_t i = 0; i < 441; i++) {
-    printf("%d\n", crossfade3_in(-50, i));
+    printf("%d\n", crossfade3_out(10, i, 0) + crossfade3_in(50, i, 0));
   }
   return 0;
 }
