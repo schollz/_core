18064d8ed3fef430408d30d49b2928038974129f
Zack Scholl <zack.scholl@gmail.com>
2023-11-22 21:57:51 +0000

    add TODO
    Former-commit-id: c1b50d6c0230bc2075e5272cb5e9962c1be6c2b8

--- lib/definitions.h
+++ lib/definitions.h
@@ -17,6 +17,16 @@ static int PHASE_DIVISOR = WAV_CHANNELS * 2;
 
 static const uint32_t PIN_DCDC_PSM_CTRL = 23;
 
+// starts at splice start and ends at splice stop
+#define PLAY_ONE_SHOT_STOP 0
+// starts at splice start, and returns to start when reaching splice boundary
+#define PLAY_ONE_SHOT_LOOP 1
+// starts at splice start and ends at sample boundary
+#define PLAY_THROUGH_STOP 2
+// starts at splice start and returns to start when reaching sample boundary
+#define PLAY_THROUGH_LOOP 3
+
+// TODO: replace the play mode below with the play mode above
 #define PLAY_MODE_CLASSIC 0
 #define PLAY_MODE_ONESHOT_GO 1
 #define PLAY_MODE_ONESHOT_STOP 2
