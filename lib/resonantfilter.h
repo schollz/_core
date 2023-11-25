0ce09e1fa9828cf2fa168ab0339f5d16223abbcc
Zack Scholl <zack.scholl@gmail.com>
2023-11-25 10:46:05 -0800

    use fixed point lib
    Former-commit-id: 23f43a6caba81f78124acc6d6404ece831758a6f

--- lib/resonantfilter.h
+++ lib/resonantfilter.h
@@ -22,53 +22,13 @@
 //
 // See http://creativecommons.org/licenses/MIT/ for more information.
 
+#include "fixedpoint.h"
 #include "resonantfilter_data.h"
 
 #define FILTER_LOWPASS 0
 #define FILTER_BANDPASS 1
 #define FILTER_HIGHPASS 2
 #define FILTER_NOTCH 3
-
-#define RESONANT_FILTER_Q_BITS 16
-#define RESONANT_FILTER_FRACTIONAL_BITS (1 << RESONANT_FILTER_Q_BITS)
-#define PI_FLOAT 3.14159265358979
-
-// Function to convert int16_t to Q8.10 fixed-point
-int32_t int16ToFixedPoint(int16_t value) {
-  return (int32_t)value << RESONANT_FILTER_Q_BITS;
-}
-
-int32_t floatToFixedPoint(float value) {
-  return (int32_t)(value * RESONANT_FILTER_FRACTIONAL_BITS);
-}
-// Function to convert Q8.10 fixed-point to int16_t
-int16_t fixedPointToInt16(int32_t fixedValue) {
-  return (int16_t)(fixedValue >> RESONANT_FILTER_Q_BITS);
-}
-
-// Function to convert a Q8.10 fixed-point value to a float
-float fixedPointToFloat(int32_t value) {
-  return (float)value / RESONANT_FILTER_FRACTIONAL_BITS;
-}
-
-int32_t multiplyFixedPoint(int32_t a, int32_t b) {
-  return (int32_t)(((int64_t)a * b) >> RESONANT_FILTER_Q_BITS);
-}
-
-// Function to divide two Q8.10 fixed-point numbers
-int32_t divideFixedPoint(int32_t numerator, int32_t denominator) {
-  // Ensure that the denominator is not zero to avoid division by zero
-  if (denominator == 0) {
-    // Handle division by zero
-    // You can choose to return an error code or a special value
-    return INT32_MAX;  // For example, return the maximum value
-  }
-
-  // Perform fixed-point division
-  int32_t result =
-      (int32_t)(((int64_t)numerator << RESONANT_FILTER_Q_BITS) / denominator);
-  return result;
-}
 
 typedef struct ResonantFilter {
   bool passthrough;
@@ -138,7 +98,7 @@ void ResonantFilter_reset(ResonantFilter* rf, uint8_t filter_type, uint8_t fc,
 
 void ResonantFilter_reset2(ResonantFilter* rf, float fc, float fs, float q,
                            float db, uint8_t filter_type) {
-  float w0 = 2 * PI_FLOAT * (fc / fs);
+  float w0 = 2 * 3.14159265358979732384626 * (fc / fs);
   float cosW = cos(w0);
   float sinW = sin(w0);
   float A = pow(10, db / 40);
@@ -169,11 +129,11 @@ void ResonantFilter_reset2(ResonantFilter* rf, float fc, float fs, float q,
   a1 = a1 / a0;
   a2 = a2 / a0;
 
-  rf->b0 = floatToFixedPoint(b0);
-  rf->b1 = floatToFixedPoint(b1);
-  rf->b2 = floatToFixedPoint(b2);
-  rf->a1 = floatToFixedPoint(a1);
-  rf->a2 = floatToFixedPoint(a2);
+  rf->b0 = q16_16_float_to_fp(b0);
+  rf->b1 = q16_16_float_to_fp(b1);
+  rf->b2 = q16_16_float_to_fp(b2);
+  rf->a1 = q16_16_float_to_fp(a1);
+  rf->a2 = q16_16_float_to_fp(a2);
 }
 
 ResonantFilter* ResonantFilter_create(uint8_t filter_type) {
@@ -183,20 +143,19 @@ ResonantFilter* ResonantFilter_create(uint8_t filter_type) {
   return rf;
 }
 
-int16_t ResonantFilter_update(ResonantFilter* rf, int16_t in) {
+int16_t ResonantFilter_update(ResonantFilter* rf, int32_t in) {
   if (rf->passthrough) {
     return in;
   }
-  int32_t x = int16ToFixedPoint(in);
-  int32_t y = multiplyFixedPoint(rf->b0, x) +
-              multiplyFixedPoint(rf->b1, rf->x1_f) +
-              multiplyFixedPoint(rf->b2, rf->x2_f) -
-              multiplyFixedPoint(rf->a1, rf->y1_f) -
-              multiplyFixedPoint(rf->a2, rf->y2_f);
+  int32_t x = q16_16_int32_to_fp(in);
+  int32_t y = q16_16_multiply(rf->b0, x) + q16_16_multiply(rf->b1, rf->x1_f) +
+              q16_16_multiply(rf->b2, rf->x2_f) -
+              q16_16_multiply(rf->a1, rf->y1_f) -
+              q16_16_multiply(rf->a2, rf->y2_f);
 
   rf->x2_f = rf->x1_f;
   rf->x1_f = x;
   rf->y2_f = rf->y1_f;
   rf->y1_f = y;
-  return fixedPointToInt16(y);
+  return q16_16_fp_to_int32(y);
 }
