5f18f62b4346e267f957fb481dae3c2e4dad2d6c
Zack Scholl <zack.scholl@gmail.com>
2023-12-02 05:15:09 -0800

    new: add way to add custom metadata
    Former-commit-id: d1266b9cd533778ab193ddbe6215c46b606e2077

---
+++ cmd/zeptocore/src/metadata/metadata.go
@@ -0,0 +1,84 @@
+package metadata
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/go-audio/wav"
+	log "github.com/schollz/logger"
+	"github.com/schollz/zeptocore/cmd/zeptocore/src/zeptocore"
+)
+
+// define error for no metadata present
+var ErrNoMetadata = fmt.Errorf("no metadata present")
+
+func SetMetadata(fname string, metadata zeptocore.File) (err error) {
+	in, err := os.Open(fname)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	d := wav.NewDecoder(in)
+	buf, err := d.FullPCMBuffer()
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	in.Close()
+
+	out, err := os.Create(fname)
+	if err != nil {
+		return
+	}
+	defer out.Close()
+
+	e := wav.NewEncoder(out,
+		buf.Format.SampleRate,
+		int(d.BitDepth),
+		buf.Format.NumChannels,
+		int(d.WavAudioFormat))
+	if err = e.Write(buf); err != nil {
+		log.Error(err)
+		return
+	}
+	e.Metadata = &wav.Metadata{}
+	b, err := json.Marshal(metadata)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	e.Metadata.Comments = string(b)
+	err = e.Close()
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	return
+}
+
+func GetMetadata(fname string) (metadata zeptocore.File, err error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	dec := wav.NewDecoder(f)
+	dec.ReadMetadata()
+	if err := dec.Err(); err != nil {
+		log.Error(err)
+	}
+	if dec.Metadata == nil {
+		err = ErrNoMetadata
+		return
+	}
+	err = json.Unmarshal([]byte(dec.Metadata.Comments), &metadata)
+	if err != nil {
+		return
+	}
+	if metadata.SliceStart == nil {
+		err = ErrNoMetadata
+		return
+	}
+	return
+}
