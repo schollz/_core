4c50bfbd000ec470062e8635faea4b9c5aa66545
Zack Scholl <zack.scholl@gmail.com>
2023-12-07 18:49:05 +0000

    new: add tape stop
    Former-commit-id: 686f727ecf57fbb9fd5be8dd438eff7505842f04

--- lib/audio_callback.h
+++ lib/audio_callback.h
@@ -45,19 +45,22 @@ void i2s_callback_func() {
     return;
   }
 
+  float envelope_pitch_val_new = Envelope2_update(envelope_pitch);
+
   int32_t *samples = (int32_t *)buffer->buffer->bytes;
 
   if (sync_using_sdcard || !fil_is_open ||
       (gate_active && gate_counter >= gate_threshold) || audio_mute ||
-      button_mute || reduce_cpu_usage > 0) {
+      button_mute || reduce_cpu_usage > 0 ||
+      (envelope_pitch_val < ENVELOPE_PITCH_THRESHOLD)) {
     if (reduce_cpu_usage > 0) {
       // printf("reduce_cpu_usage: %d\n", reduce_cpu_usage);
       reduce_cpu_usage--;
     }
     for (uint16_t i = 0; i < buffer->max_sample_count; i++) {
       int32_t value0 = 0;
-      samples[i * 2 + 0] = value0 + (value0 >> 16u); // L
-      samples[i * 2 + 1] = samples[i * 2 + 0];       // R = L
+      samples[i * 2 + 0] = value0 + (value0 >> 16u);  // L
+      samples[i * 2 + 1] = samples[i * 2 + 0];        // R = L
     }
     buffer->sample_count = buffer->max_sample_count;
     give_audio_buffer(ap, buffer);
@@ -70,7 +73,8 @@ void i2s_callback_func() {
     return;
   }
 
-  if (trigger_button_mute) {
+  envelope_pitch_val = envelope_pitch_val_new;
+  if (trigger_button_mute || envelope_pitch_val < ENVELOPE_PITCH_THRESHOLD) {
     do_fade_out = true;
   }
 
@@ -83,11 +87,11 @@ void i2s_callback_func() {
     bool do_gate_down = false;
     if (gate_is_applied && gate_counter == 0) {
       gate_is_applied = false;
-      do_gate_up = true; // allow the sound to come through
+      do_gate_up = true;  // allow the sound to come through
     } else if (gate_active) {
       gate_counter++;
       if (!gate_is_applied && gate_counter >= gate_threshold) {
-        do_gate_down = true; // mute the sound
+        do_gate_down = true;  // mute the sound
       }
     }
 
@@ -136,12 +140,6 @@ void i2s_callback_func() {
       }
     }
 
-    // determine the samples to read
-    envelope_pitch_val = Envelope2_update(envelope_pitch);
-    // TODO: switch for if wobble is enabled
-    // envelope_pitch_val =
-    //     envelope_pitch_val * Range(LFNoise2(noise_wobble, 1), 0.9, 1.1);
-
     // check if tempo matching is activated, if not then don't change
     // based on bpm
     uint32_t samples_to_read;
@@ -171,7 +169,7 @@ void i2s_callback_func() {
                                                 ->sample[sel_sample_cur]
                                                 .snd[sel_variation]
                                                 ->num_channels;
-    uint32_t values_to_read = values_len * 2; // 16-bit = 2 x 1 byte reads
+    uint32_t values_to_read = values_len * 2;  // 16-bit = 2 x 1 byte reads
     int16_t values[values_len];
     uint vol_main =
         (uint)round(sf->vol * retrig_vol * Envelope2_update(envelope3));
@@ -200,51 +198,51 @@ void i2s_callback_func() {
                   ->sample[sel_sample_cur]
                   .snd[sel_variation]
                   ->play_mode) {
-      case PLAY_NORMAL:
-        if (phase_forward && phases[0] > sample_stop) {
-          phase_change = true;
-          phase_new = phases[0] - sample_stop;
-        } else if (!phase_forward && phases[0] < 0) {
-          phase_change = true;
-          phase_new = phases[0] + sample_stop;
-        }
-        break;
-      case PLAY_SPLICE_STOP:
-        if ((phase_forward && (next_phase > splice_stop)) ||
-            (!phase_forward && (next_phase < splice_start))) {
-          do_fade_out = true;
-        }
-        break;
-      case PLAY_SPLICE_LOOP:
-        if (phase_forward && (phases[0] > splice_stop)) {
-          phase_change = true;
-          phase_new = splice_start;
-        } else if (!phase_forward && (phases[0] < splice_stop)) {
-          phase_change = true;
-          phase_new = splice_stop;
-        }
-        break;
-      case PLAY_SAMPLE_STOP:
-        if ((phase_forward && (next_phase > sample_stop)) ||
-            (!phase_forward && (next_phase < 0))) {
-          do_fade_out = true;
-        }
-        break;
-      case PLAY_SAMPLE_LOOP:
-        if (phase_forward && (phases[0] > sample_stop)) {
-          phase_change = true;
-          phase_new = splice_start;
-        } else if (!phase_forward && (phases[0] < 0)) {
-          phase_change = true;
-          phase_new = splice_stop;
-        }
-        break;
+        case PLAY_NORMAL:
+          if (phase_forward && phases[0] > sample_stop) {
+            phase_change = true;
+            phase_new = phases[0] - sample_stop;
+          } else if (!phase_forward && phases[0] < 0) {
+            phase_change = true;
+            phase_new = phases[0] + sample_stop;
+          }
+          break;
+        case PLAY_SPLICE_STOP:
+          if ((phase_forward && (next_phase > splice_stop)) ||
+              (!phase_forward && (next_phase < splice_start))) {
+            do_fade_out = true;
+          }
+          break;
+        case PLAY_SPLICE_LOOP:
+          if (phase_forward && (phases[0] > splice_stop)) {
+            phase_change = true;
+            phase_new = splice_start;
+          } else if (!phase_forward && (phases[0] < splice_stop)) {
+            phase_change = true;
+            phase_new = splice_stop;
+          }
+          break;
+        case PLAY_SAMPLE_STOP:
+          if ((phase_forward && (next_phase > sample_stop)) ||
+              (!phase_forward && (next_phase < 0))) {
+            do_fade_out = true;
+          }
+          break;
+        case PLAY_SAMPLE_LOOP:
+          if (phase_forward && (phases[0] > sample_stop)) {
+            phase_change = true;
+            phase_new = splice_start;
+          } else if (!phase_forward && (phases[0] < 0)) {
+            phase_change = true;
+            phase_new = splice_stop;
+          }
+          break;
       }
     }
 
     if (phase_change) {
       do_crossfade = true;
-      phases[1] = phases[0]; // old phase
+      phases[1] = phases[0];  // old phase
       phases[0] = phase_new;
       phase_change = false;
     }
@@ -311,8 +309,8 @@ void i2s_callback_func() {
           printf("problem seeking to phase (%d)\n", phases[head]);
           for (uint16_t i = 0; i < buffer->max_sample_count; i++) {
             int32_t value0 = 0;
-            samples[i * 2 + 0] = value0 + (value0 >> 16u); // L
-            samples[i * 2 + 1] = samples[i * 2 + 0];       // R = L
+            samples[i * 2 + 0] = value0 + (value0 >> 16u);  // L
+            samples[i * 2 + 1] = samples[i * 2 + 0];        // R = L
           }
           buffer->sample_count = buffer->max_sample_count;
           give_audio_buffer(ap, buffer);
@@ -325,7 +323,7 @@ void i2s_callback_func() {
       t0 = time_us_32();
       if (f_read(&fil_current, values, values_to_read, &fil_bytes_read)) {
         printf("ERROR READING!\n");
-        f_close(&fil_current); // close and re-open trick
+        f_close(&fil_current);  // close and re-open trick
         f_open(&fil_current,
                banks[sel_bank_cur]
                    ->sample[sel_sample_cur]
@@ -418,13 +416,13 @@ void i2s_callback_func() {
             if (head == 0) {
               samples[i * 2 + 0] = (vol_main * samples[i * 2 + 0]) << 8u;
               samples[i * 2 + 0] += (samples[i * 2 + 0] >> 16u);
-              samples[i * 2 + 1] = samples[i * 2 + 0]; // R = L
+              samples[i * 2 + 1] = samples[i * 2 + 0];  // R = L
             }
           } else {
             samples[i * 2 + 0] += newArray[i];
             samples[i * 2 + 0] = (vol_main * samples[i * 2 + 0]) << 8u;
             samples[i * 2 + 0] += (samples[i * 2 + 0] >> 16u);
-            samples[i * 2 + 1] = samples[i * 2 + 0]; // R = L
+            samples[i * 2 + 1] = samples[i * 2 + 0];  // R = L
           }
           // int32_t value0 = (vol * newArray[i]) << 8u;
           // samples[i * 2 + 0] =
@@ -440,7 +438,7 @@ void i2s_callback_func() {
                      ->num_channels == 2) {
         // stereo
         for (uint8_t channel = 0; channel < 2; channel++) {
-          int16_t valuesC[samples_to_read]; // max limit
+          int16_t valuesC[samples_to_read];  // max limit
           for (uint16_t i = 0; i < values_len; i++) {
             if (i % 2 == channel) {
               valuesC[i / 2] = values[i];
