0ba8171aafd6e38468113c732829a7034bc97833
Zack Scholl <zack.scholl@gmail.com>
2023-12-07 17:51:12 +0000

    chore: check sin01 works
    Former-commit-id: f2a40cfbedac014891e0d8b89ae1b25fecdecbf1

--- lib/test/fixedpoint/main.c
+++ lib/test/fixedpoint/main.c
@@ -34,12 +34,15 @@ int main() {
   int32_t b = q16_16_float_to_fp(-0.25);
   printf("a * b = %d\n", q16_16_fp_to_int32(q16_16_multiply(a, b)));
 
-  int32_t v = 75000;
+  int32_t v = -75000;
 
   printf("%d\n", q16_16_multiply(v, q16_16_float_to_fp(0.5123)));
 
-  for (float i = 0.0; i < 3.1415 * 2; i += 0.25) {
-    printf("%d\n", q16_16_multiply(v, q16_16_sin01(q16_16_float_to_fp(i))));
+  int32_t start = 0;
+  for (int32_t i = start; i < start + Q16_16_2PI; i += Q16_16_2PI / 96) {
+    int32_t l = q16_16_multiply(v, q16_16_sin01(i));
+    int32_t r = q16_16_multiply(v, Q16_16_1 - q16_16_sin01(i));
+    printf("%d %d\n", l, r);
   }
 
   return 0;
