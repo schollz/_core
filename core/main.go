faadde7b9b14b26d172032102237211887b54bc0
Zack Scholl <zack.scholl@gmail.com>
2023-12-23 07:27:10 -0800

    new: cmd opens browser on double-click
    Former-commit-id: 6d3a253f85cb5c747bddc6feaa13c77381844603

--- core/main.go
+++ core/main.go
@@ -4,18 +4,23 @@ import (
 	"flag"
 
 	"github.com/schollz/_core/core/src/server"
+	"github.com/schollz/_core/core/src/utils"
 	log "github.com/schollz/logger"
 )
 
 var flagLogLevel string
+var flagDontOpen bool
 
 func init() {
 	flag.StringVar(&flagLogLevel, "log", "debug", "log level (trace, debug, info)")
+	flag.BoolVar(&flagDontOpen, "dontopen", false, "don't open browser")
 }
 
 func main() {
 	flag.Parse()
 	log.SetLevel(flagLogLevel)
-	log.SetLevel("trace")
+	if !flagDontOpen {
+		utils.OpenBrowser("http://localhost:8101")
+	}
 	server.Serve()
 }
--- core/src/utils/utils.go
+++ core/src/utils/utils.go
@@ -6,10 +6,30 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 
 	log "github.com/schollz/logger"
 )
+
+func OpenBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Error(err)
+	}
+
+}
 
 func Run(args ...string) (string, string, error) {
 	log.Trace(strings.Join(args, " "))
