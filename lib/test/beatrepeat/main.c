cc504176f8c53b0555af7ed294e37099bfc00891
Zack Scholl <zack.scholl@gmail.com>
2023-12-25 14:42:21 -0800

    fix: beatrepeat needs zero crossings
    Former-commit-id: 1c0d31b17d99bc3b8f16d9bcd0b21913af0200b1

--- lib/test/beatrepeat/main.c
+++ lib/test/beatrepeat/main.c
@@ -39,19 +39,25 @@ int main() {
   SinOsc_wave(osc2, 29);
   SinOsc_quiet(osc2, 1);
   BeatRepeat *br = BeatRepeat_malloc();
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < 2000; i++) {
     int16_t val = (SinOsc_next(osc1) >> 17) + (SinOsc_next(osc2) >> 17);
     BeatRepeat_process(br, val);
     printf("%d\n", val);
   }
-  BeatRepeat_repeat(br, 200);
-  for (int i = 0; i < 1000; i++) {
+  BeatRepeat_repeat(br, 300);
+  for (int i = 0; i < 4000; i++) {
     int16_t val = (SinOsc_next(osc1) >> 17) + (SinOsc_next(osc2) >> 17);
     val = BeatRepeat_process(br, val);
     printf("%d\n", val);
   }
   BeatRepeat_repeat(br, 0);
-  for (int i = 0; i < 1200; i++) {
+  for (int i = 0; i < 4000; i++) {
+    int16_t val = (SinOsc_next(osc1) >> 17) + (SinOsc_next(osc2) >> 17);
+    val = BeatRepeat_process(br, val);
+    printf("%d\n", val);
+  }
+  BeatRepeat_repeat(br, 1350);
+  for (int i = 0; i < 4000; i++) {
     int16_t val = (SinOsc_next(osc1) >> 17) + (SinOsc_next(osc2) >> 17);
     val = BeatRepeat_process(br, val);
     printf("%d\n", val);
