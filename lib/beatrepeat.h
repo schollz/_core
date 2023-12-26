1544f6abb5129f561bb68e5a84e43f56ab3e2497
Zack Scholl <zack.scholl@gmail.com>
2023-12-25 19:13:48 -0800

    fix: beatrepeat
    Former-commit-id: 5942ad684a25bbaa6b46d57590cb97fe9af7787b

--- lib/beatrepeat.h
+++ lib/beatrepeat.h
@@ -24,17 +24,16 @@
 
 #ifndef BEATREPEAT_LIB
 #define BEATREPEAT_LIB 1
-#define BEATREPEAT_RINGBUFFER_SIZE 44100
-#define BEATREPEAT_ZEROCROSSING_SIZE 10000
+#define BEATREPEAT_RINGBUFFER_SIZE 22050
+#define BEATREPEAT_ZEROCROSSING_SIZE 1000
 #include "fixedpoint.h"
 //
 #include "crossfade3.h"
 #include "stdbool.h"
-int16_t beatrepeat_ringbuffer[BEATREPEAT_RINGBUFFER_SIZE];
-int16_t beatrepeat_zerocrossings[BEATREPEAT_ZEROCROSSING_SIZE];
 
 typedef struct BeatRepeat {
   // buffer for holding original samples
+  int16_t ringbuffer[BEATREPEAT_RINGBUFFER_SIZE];
   int16_t ringbuffer_index;
   // repeat state
   int16_t repeat_start;
@@ -44,6 +43,7 @@ typedef struct BeatRepeat {
   int16_t crossfade_in;
   int16_t crossfade_out;
   // zerocrossings for selecting best repeat
+  int16_t zerocrossings[BEATREPEAT_ZEROCROSSING_SIZE];
   int16_t zerocrossings_index;
   int16_t last;
 } BeatRepeat;
@@ -52,7 +52,7 @@ BeatRepeat *BeatRepeat_malloc() {
   BeatRepeat *self = (BeatRepeat *)malloc(sizeof(BeatRepeat));
   self->ringbuffer_index = 0;
   for (int i = 0; i < BEATREPEAT_RINGBUFFER_SIZE; i++) {
-    beatrepeat_ringbuffer[i] = 0;
+    self->ringbuffer[i] = 0;
   }
   self->repeat_start = -1;
   self->repeat_end = -1;
@@ -62,7 +62,7 @@ BeatRepeat *BeatRepeat_malloc() {
 
   self->zerocrossings_index = 0;
   for (int i = 0; i < BEATREPEAT_ZEROCROSSING_SIZE; i++) {
-    beatrepeat_zerocrossings[i] = -1;
+    self->zerocrossings[i] = -1;
   }
   return self;
 }
@@ -73,7 +73,7 @@ void BeatRepeat_process(BeatRepeat *self, int16_t *samples,
     int16_t sample = samples[ii];
     // do beat repeat
     if (self->repeat_start > -1 && self->repeat_end > -1) {
-      int16_t sample2 = beatrepeat_ringbuffer[self->repeat_index];
+      int16_t sample2 = self->ringbuffer[self->repeat_index];
       if (self->crossfade_in < CROSSFADE3_LIMIT) {
         sample = q16_16_fp_to_int16(
             q16_16_multiply(crossfade3_line[self->crossfade_in],
@@ -107,14 +107,13 @@ void BeatRepeat_process(BeatRepeat *self, int16_t *samples,
     } else {
       // do zerocrossing detection
       if (self->last < 0 && sample >= 0) {
-        beatrepeat_zerocrossings[self->zerocrossings_index] =
-            self->ringbuffer_index;
+        self->zerocrossings[self->zerocrossings_index] = self->ringbuffer_index;
         self->zerocrossings_index++;
         if (self->zerocrossings_index > BEATREPEAT_ZEROCROSSING_SIZE) {
           self->zerocrossings_index = 0;
         }
       }
-      beatrepeat_ringbuffer[self->ringbuffer_index] = sample;
+      self->ringbuffer[self->ringbuffer_index] = sample;
       self->ringbuffer_index++;
       if (self->ringbuffer_index > BEATREPEAT_RINGBUFFER_SIZE) {
         self->ringbuffer_index = 0;
@@ -138,15 +137,15 @@ void BeatRepeat_repeat(BeatRepeat *self, int16_t num_samples) {
   self->crossfade_in = 0;
   self->crossfade_out = CROSSFADE3_LIMIT;
 
-  self->repeat_end = beatrepeat_zerocrossings[self->zerocrossings_index - 1];
+  self->repeat_end = self->zerocrossings[self->zerocrossings_index - 1];
   // find the first repeat_start that exceeds the requested num_samples
   for (int j = 0; j < BEATREPEAT_ZEROCROSSING_SIZE; j++) {
     int i = self->zerocrossings_index - j - 1;
     if (i < 0) {
       i += BEATREPEAT_ZEROCROSSING_SIZE;
     }
-    if (beatrepeat_zerocrossings[i] > -1) {
-      self->repeat_start = beatrepeat_zerocrossings[i];
+    if (self->zerocrossings[i] > -1) {
+      self->repeat_start = self->zerocrossings[i];
       int16_t diff = self->repeat_end - self->repeat_start;
       if (diff < 0) {
         diff += BEATREPEAT_RINGBUFFER_SIZE;
