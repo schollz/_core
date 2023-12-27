8ed0bf1aed7a03c786043d6d3c2e4f6abc6dccb3
Zack Scholl <zack.scholl@gmail.com>
2023-12-27 23:03:42 +0000

    works better
    Former-commit-id: c2744ffb8d959cc20864cc1081b3834dc38dc2d1

--- lib/test/bitfields_go/main.go
+++ lib/test/bitfields_go/main.go
@@ -11,26 +11,45 @@ import (
 )
 
 func main() {
-	arr := []byte{7, 3}
-	arrPtr := (*C.uchar)(unsafe.Pointer(&arr[0]))
-	ret := C.writeStructToFile(4, 5, 2, arrPtr)
+	sliceStart := []int32{10, 20, 30}
+	sliceStartPtr := (*C.int)(unsafe.Pointer(&sliceStart[0]))
+	sliceStop := []int32{100, 200, 300}
+	sliceStopPtr := (*C.int)(unsafe.Pointer(&sliceStop[0]))
+	bpm := 120
+	beats := 8
+	play_mode := 0
+	splice_trigger := 3
+	tempo_match := 1
+	oversampling := 1
+	num_channels := 0
+	cStruct := C.SampleInfo_malloc(C.uint(bpm), C.uint(beats), C.uchar(play_mode), C.uchar(splice_trigger), C.uchar(tempo_match), C.uchar(oversampling), C.uchar(num_channels),
+		C.uint(len(sliceStart)), sliceStartPtr, sliceStopPtr)
+	fmt.Println("SampleInfo_getBPM", C.SampleInfo_getBPM(cStruct))
+	fmt.Println("SampleInfo_getSliceNum", C.SampleInfo_getSliceNum(cStruct))
+	for i := range sliceStart {
+		fmt.Println("SampleInfo_getSliceStart", i, C.SampleInfo_getSliceStart(cStruct, C.ushort(i)))
+	}
+	defer C.SampleInfo_free(cStruct)
+
+	ret := C.SampleInfo_writeToDisk(cStruct)
 	if ret != 0 {
 		fmt.Println("Failed to write struct to file")
-	} else {
-		fmt.Println("Struct written to file successfully")
+		return
 	}
 
-	cStruct := C.readStructFromFile()
-	if cStruct == nil {
+	cStruct2 := C.SampleInfo_readFromDisk()
+	if cStruct2 == nil {
 		fmt.Println("Failed to read struct from file")
 		return
 	}
-	defer C.free(unsafe.Pointer(cStruct))
+	defer C.free(unsafe.Pointer(cStruct2))
+	fmt.Println("SampleInfo_getBPM", C.SampleInfo_getBPM(cStruct2))
+	fmt.Println("SampleInfo_getSliceNum", C.SampleInfo_getSliceNum(cStruct2))
+	for i := range sliceStart {
+		fmt.Println("SampleInfo_getSliceStart", i, C.SampleInfo_getSliceStart(cStruct2, C.ushort(i)))
+	}
+	for i := range sliceStart {
+		fmt.Println("SampleInfo_getSliceStop", i, C.SampleInfo_getSliceStop(cStruct2, C.ushort(i)))
+	}
 
-	// Using accessor functions to get the values of a and b
-	a := C.getA(cStruct)
-	b := C.getB(cStruct)
-	c := C.getArr(cStruct, 0)
-	d := C.getArr(cStruct, 1)
-	fmt.Printf("Read struct from file: a = %d, b = %d, c= %d, d = %d\n", a, b, c, d)
 }
