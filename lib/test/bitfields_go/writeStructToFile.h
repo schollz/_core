652a0c32585f3272d717237c53534e5053903254
Zack Scholl <zack.scholl@gmail.com>
2023-12-27 18:57:15 +0000

    works with arrays
    Former-commit-id: 0090132e32a04d9771f7be9736e079e2b5c23390

--- lib/test/bitfields_go/writeStructToFile.h
+++ lib/test/bitfields_go/writeStructToFile.h
@@ -9,9 +9,11 @@
 typedef struct SomeStruct {
   uint8_t a : 4;
   uint8_t b : 4;
+  uint8_t num;
+  uint8_t *arr;
 } SomeStruct;
 
-int writeStructToFile(uint8_t a, uint8_t b) {
+int writeStructToFile(uint8_t a, uint8_t b, uint8_t num, uint8_t *arr) {
   SomeStruct *data = malloc(sizeof(SomeStruct));
   if (data == NULL) {
     perror("Error allocating memory");
@@ -20,7 +22,11 @@ int writeStructToFile(uint8_t a, uint8_t b) {
 
   data->a = a;
   data->b = b;
-
+  data->num = num;
+  data->arr = (uint8_t *)malloc(sizeof(uint8_t) * num);
+  for (uint8_t i = 0; i < num; i++) {
+    data->arr[i] = arr[i];
+  }
   FILE *file = fopen("test.bin", "wb");
   if (file == NULL) {
     perror("Error opening file");
@@ -28,14 +34,26 @@ int writeStructToFile(uint8_t a, uint8_t b) {
     return -1;
   }
 
-  if (fwrite(data, sizeof(SomeStruct), 1, file) != 1) {
+  // Write the struct (excluding the array)
+  if (fwrite(data, sizeof(SomeStruct) - sizeof(uint8_t *), 1, file) != 1) {
     perror("Error writing struct to file");
     fclose(file);
+    free(data->arr);
+    free(data);
+    return -1;
+  }
+
+  // Write the array content
+  if (fwrite(data->arr, sizeof(uint8_t), num, file) != num) {
+    perror("Error writing array to file");
+    fclose(file);
+    free(data->arr);
     free(data);
     return -1;
   }
 
   fclose(file);
+  free(data->arr);
   free(data);
   return 0;
 }
@@ -54,9 +72,28 @@ SomeStruct *readStructFromFile() {
     return NULL;
   }
 
-  if (fread(data, sizeof(SomeStruct), 1, file) != 1) {
+  // Read the struct (excluding the array pointer)
+  if (fread(data, sizeof(SomeStruct) - sizeof(uint8_t *), 1, file) != 1) {
     perror("Error reading struct from file");
     fclose(file);
+    free(data);
+    return NULL;
+  }
+
+  // Allocate memory for the array
+  data->arr = (uint8_t *)malloc(sizeof(uint8_t) * data->num);
+  if (data->arr == NULL) {
+    perror("Error allocating memory for array");
+    fclose(file);
+    free(data);
+    return NULL;
+  }
+
+  // Read the array content
+  if (fread(data->arr, sizeof(uint8_t), data->num, file) != data->num) {
+    perror("Error reading array from file");
+    fclose(file);
+    free(data->arr);
     free(data);
     return NULL;
   }
@@ -68,4 +105,6 @@ SomeStruct *readStructFromFile() {
 uint8_t getA(SomeStruct *s) { return s->a; }
 
 uint8_t getB(SomeStruct *s) { return s->b; }
+
+uint8_t getArr(SomeStruct *s, uint8_t i) { return s->arr[i]; }
 #endif  // WRITESTRUCTTOFILE_H
