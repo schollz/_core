9b490401ea3273f666724690fd9bdd152e2c6a22
Zack Scholl <zack.scholl@gmail.com>
2023-12-28 11:45:44 -0800

    refactor: delay does int32_t numbers
    Former-commit-id: 1a1df5bb18cfe1f172d0a6568f6b6d2ddc686982

--- lib/delay.h
+++ lib/delay.h
@@ -24,16 +24,16 @@
 
 #ifndef DELAY_LIB
 #define DELAY_LIB 1
-#define DELAY_RINGBUFFER_SIZE 22050
+#define DELAY_RINGBUFFER_SIZE 11025
 #define DELAY_ZEROCROSSING_SIZE 10000
 #include "fixedpoint.h"
 //
 #include "crossfade3.h"
 #include "stdbool.h"
-int16_t delay_ringbuffer[DELAY_RINGBUFFER_SIZE];
 
 typedef struct Delay {
   // buffer for holding original samples
+  int32_t delay_ringbuffer[DELAY_RINGBUFFER_SIZE];
   uint16_t ringbuffer_index;
   uint8_t feedback;  // (1-16)
   uint16_t duration;
@@ -48,53 +48,62 @@ Delay *Delay_malloc() {
   Delay *self = (Delay *)malloc(sizeof(Delay));
   self->ringbuffer_index = 0;
   self->feedback = 1;
-  self->duration = 22050;
+  self->duration = 11025;
   self->on = false;
   self->crossfade_in = CROSSFADE3_LIMIT;
   self->crossfade_out = CROSSFADE3_LIMIT;
   for (int i = 0; i < DELAY_RINGBUFFER_SIZE; i++) {
-    delay_ringbuffer[i] = 0;
+    self->delay_ringbuffer[i] = 0;
   }
   return self;
 }
 
 void Delay_setDuration(Delay *self, uint16_t num_samples) {
+  if (num_samples > DELAY_RINGBUFFER_SIZE) {
+    num_samples = DELAY_RINGBUFFER_SIZE;
+  }
   self->duration = num_samples;
 }
 
-void Delay_process(Delay *self, int16_t *samples, uint16_t num_samples) {
+void Delay_process(Delay *self, int32_t *samples, uint16_t num_samples,
+                   uint8_t channel) {
   for (int ii = 0; ii < num_samples; ii++) {
     while (self->ringbuffer_index > self->duration) {
       self->ringbuffer_index -= self->duration;
     }
-    delay_ringbuffer[self->ringbuffer_index] =
-        delay_ringbuffer[self->ringbuffer_index] >> self->feedback;
+    self->delay_ringbuffer[self->ringbuffer_index] =
+        self->delay_ringbuffer[self->ringbuffer_index] >> self->feedback;
     self->ringbuffer_index++;
     if (self->ringbuffer_index == self->duration) {
       self->ringbuffer_index = 0;
     }
 
     if (self->crossfade_in < CROSSFADE3_LIMIT) {
-      samples[ii] += q16_16_fp_to_int16(q16_16_multiply(
-          Q16_16_1 - crossfade3_line[self->crossfade_in],
-          q16_16_int16_to_fp(delay_ringbuffer[self->ringbuffer_index])));
+      int32_t v =
+          q16_16_multiply(Q16_16_1 - crossfade3_line[self->crossfade_in],
+                          self->delay_ringbuffer[self->ringbuffer_index]);
+      samples[ii * 2 + 0] += v;
+      samples[ii * 2 + 1] += v;
       self->crossfade_in++;
       if (self->crossfade_in == CROSSFADE3_LIMIT) {
         self->on = true;
       }
     } else if (self->crossfade_out < CROSSFADE3_LIMIT) {
-      samples[ii] += q16_16_fp_to_int16(q16_16_multiply(
-          crossfade3_line[self->crossfade_out],
-          q16_16_int16_to_fp(delay_ringbuffer[self->ringbuffer_index])));
+      int32_t v =
+          q16_16_multiply(crossfade3_line[self->crossfade_out],
+                          self->delay_ringbuffer[self->ringbuffer_index]);
+      samples[ii * 2 + 0] += v;
+      samples[ii * 2 + 1] += v;
       self->crossfade_out++;
       if (self->crossfade_out >= CROSSFADE3_LIMIT) {
         self->on = false;
       }
     } else if (self->on) {
-      samples[ii] += delay_ringbuffer[self->ringbuffer_index];
+      samples[ii * 2 + 0] += self->delay_ringbuffer[self->ringbuffer_index];
+      samples[ii * 2 + 1] += self->delay_ringbuffer[self->ringbuffer_index];
     }
 
-    delay_ringbuffer[self->ringbuffer_index] = samples[ii];
+    self->delay_ringbuffer[self->ringbuffer_index] = samples[ii * 2 + channel];
   }
 }
 
