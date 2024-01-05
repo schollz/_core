517a3c5bc0ed23fcf8b8ff3de33882b2a221f3bb
Zack Scholl <zack.scholl@gmail.com>
2024-01-05 06:56:15 -0800

    fix: show timestretch position
    Former-commit-id: 205f1711c8b6d5e277d228438bb0a0cec37a637a

--- lib/test/utils/main.c
+++ lib/test/utils/main.c
@@ -12,5 +12,8 @@ int main() {
          linlin_uint16_t(x, 0, 30, 0, 60000));
   printf("map(%d, 0, 30, 0, 888888) = %d\n", x,
          linlin_uint32_t(x, 0, 30, 0, 888888));
+  printf("map(%d, 0, 255, 0, 100) = %d\n", x, linlin(x, 0, 255, 0, 100));
+  printf("map(%d, 0, 30, 0, 60000) = %d\n", x, linlin(x, 0, 30, 0, 60000));
+  printf("map(%d, 0, 30, 0, 888888) = %d\n", x, linlin(x, 0, 30, 0, 888888));
   return 0;
 }
