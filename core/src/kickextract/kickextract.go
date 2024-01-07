8f7a182ed09a58ecb4fcd3e5d0fa211f27775bc9
Zack Scholl <zack.scholl@gmail.com>
2024-01-07 09:22:55 -0800

    kickextract goes in 260ms
    Former-commit-id: 0793a8ed031640080b12083f8931b27bdb60d70f

--- core/src/kickextract/kickextract.go
+++ core/src/kickextract/kickextract.go
@@ -6,11 +6,15 @@ import (
 	"math"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/schollz/_core/core/src/sox"
 	log "github.com/schollz/logger"
 )
+
+const NUM_FREQUNCIES = 435
+const KICK_FREQUNCIES = 204
 
 func KickExtract(fname string, slice_start []float64, slice_end []float64) (kicks []bool, err error) {
 	totalSamplesI, err := sox.NumSamples(fname)
@@ -19,7 +23,6 @@ func KickExtract(fname string, slice_start []float64, slice_end []float64) (kick
 	}
 	totalSamples := float64(totalSamplesI)
 	kicks = make([]bool, len(slice_start))
-	fnames := make([]string, len(slice_start))
 	for i := 0; i < len(slice_start); i++ {
 		// create temp file
 		var f *os.File
@@ -28,12 +31,54 @@ func KickExtract(fname string, slice_start []float64, slice_end []float64) (kick
 			return
 		}
 		f.Close()
-		fnames[i] = f.Name() + ".wav"
-		run("sox", fname, fnames[i], "trim", fmt.Sprintf("%2.0fs", math.Round(totalSamples*slice_start[i])), fmt.Sprintf("%2.0fs", math.Round(totalSamples*(slice_end[i]-slice_start[i]))))
-		os.Remove(fnames[i])
-	}
-	for i := 0; i < len(slice_start); i++ {
-		fmt.Println(fnames[i])
+		fname2 := f.Name() + ".wav"
+		defer os.Remove(fname2)
+		_, _, err = run("sox", fname, fname2, "trim", fmt.Sprintf("%2.0fs", math.Round(totalSamples*slice_start[i])), fmt.Sprintf("%2.0fs", math.Round(totalSamples*(slice_end[i]-slice_start[i]))))
+		if err != nil {
+			return
+		}
+		var dataString string
+		_, dataString, err = run("sox", fname2, "-n", "stat", "-freq")
+		if err != nil {
+			return
+		}
+		datas := make(map[int][]float64)
+		for i := 0; i < 435; i++ {
+			datas[i] = []float64{}
+		}
+		for _, line := range strings.Split(dataString, "\n") {
+			fields := strings.Fields(line)
+			if len(fields) != 2 {
+				continue
+			}
+			freq, _ := strconv.ParseFloat(fields[0], 64)
+			freq = math.Round(math.Log10(freq) * 100)
+			freqI := int(freq)
+			if freqI < 0 || freqI > NUM_FREQUNCIES {
+				continue
+			}
+			power, _ := strconv.ParseFloat(fields[1], 64)
+			datas[freqI] = append(datas[freqI], power)
+		}
+
+		dataMean := make([]float64, NUM_FREQUNCIES)
+		for i := 0; i < NUM_FREQUNCIES; i++ {
+			dataMean[i] = 0
+			if len(datas[i]) > 0 {
+				sum := 0.0
+				num := 0.0
+				for _, v := range datas[i] {
+					sum += v
+					num++
+				}
+				dataMean[i] = sum / num
+			}
+		}
+		kickValue := 0.0
+		for i := 0; i < KICK_FREQUNCIES; i++ {
+			kickValue += dataMean[i]
+		}
+		fmt.Println(kickValue)
 	}
 
 	return
