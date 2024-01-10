9c38c3a78077b4bb7dfbe64289cd59c9fe2b1e46
Zack Scholl <zack.scholl@gmail.com>
2024-01-10 09:12:49 -0800

    feat: wavetable
    Former-commit-id: 9fc5ef69d804cfc69282aea9fa4775749ff20782

--- lib/wavetableosc.h
+++ lib/wavetableosc.h
@@ -7,52 +7,26 @@
 #include "wavetable_data.h"
 
 typedef struct WaveOsc {
-  uint8_t wave[2];
-  uint16_t phase[2];
-  uint16_t limit[2];
-  uint8_t quiet[2];
+  uint8_t wave;
+  uint16_t phase;
+  uint16_t limit;
+  uint8_t quiet;
+  bool fade_in;
+  bool fade_out;
   uint16_t crossfade;
   uint8_t crossfade_factor;
 } WaveOsc;
 
-void WaveOsc_wave(WaveOsc *self, uint8_t wave) {
-  if (self->wave[0] == wave) {
-    return;
-  }
-  if (wave >= 0 && wave < WAVETABLEOSC_MAX) {
-    self->crossfade = 0;
-    self->quiet[1] = self->quiet[0];
-    self->wave[1] = self->wave[0];
-    self->phase[1] = self->phase[0];
-    self->limit[1] = self->limit[0];
-    self->wave[0] = wave;
-    self->phase[0] = 0;
-    self->limit[0] = sinewave_len(wave);
-  }
-}
-
-void WaveOsc_quiet(WaveOsc *self, uint8_t quiet) {
-  if (quiet >= 0 && quiet <= 16) {
-    self->crossfade = 0;
-    self->quiet[1] = self->quiet[0];
-    self->wave[1] = self->wave[0];
-    self->phase[1] = self->phase[0];
-    self->limit[1] = self->limit[0];
-    self->quiet[0] = quiet;
-  }
-}
-
-WaveOsc *WaveOsc_malloc() {
+WaveOsc *WaveOsc_malloc(uint8_t wave, uint8_t quiet) {
   WaveOsc *self = malloc(sizeof(WaveOsc));
-  self->quiet[0] = 0;
-  self->quiet[1] = 0;
-  self->wave[0] = 0;
-  self->wave[1] = 0;
-  self->phase[0] = 0;
-  self->phase[1] = 0;
-  self->limit[0] = sinewave_len(0);
-  self->limit[1] = sinewave_len(0);
-  self->crossfade = CROSSFADE3_LIMIT;
+  self->wave = wave;
+  self->quiet = quiet;
+  self->phase = 0;
+  self->limit = wavetable_len(wave);
+  self->fade_in = true;
+  self->fade_out = false;
+  self->crossfade = 0;
+  self->crossfade_factor = 0;
   return self;
 }
 
@@ -60,6 +34,7 @@ void WaveOsc_free(WaveOsc *self) { free(self); }
 
 int32_t WaveOsc_next(WaveOsc *self) {
   int32_t val = 0;
+
   if (self->crossfade < CROSSFADE3_LIMIT) {
     if (self->phase[0] >= self->limit[0]) {
       self->phase[0] = 0;
@@ -69,7 +44,7 @@ int32_t WaveOsc_next(WaveOsc *self) {
     }
     if (self->wave[0] == 0) {
       val = q16_16_multiply(crossfade3_cos_out[self->crossfade],
-                            sinewave_sample(self->wave[1], self->phase[1])) >>
+                            wavetable_sample(self->wave[1], self->phase[1])) >>
             self->quiet[1];
       if (self->crossfade_factor == 0) {
         self->crossfade++;
@@ -79,16 +54,16 @@ int32_t WaveOsc_next(WaveOsc *self) {
       }
     } else if (self->wave[1] == 0) {
       val = q16_16_multiply(Q16_16_1 - crossfade3_line[self->crossfade],
-                            sinewave_sample(self->wave[0], self->phase[0])) >>
+                            wavetable_sample(self->wave[0], self->phase[0])) >>
             self->quiet[0];
 
       self->crossfade++;
     } else {
       val = (q16_16_multiply(Q16_16_1 - crossfade3_line[self->crossfade],
-                             sinewave_sample(self->wave[0], self->phase[0])) >>
+                             wavetable_sample(self->wave[0], self->phase[0])) >>
              self->quiet[0]) +
             (q16_16_multiply(crossfade3_line[self->crossfade],
-                             sinewave_sample(self->wave[1], self->phase[1])) >>
+                             wavetable_sample(self->wave[1], self->phase[1])) >>
              self->quiet[1]);
       self->crossfade++;
     }
@@ -98,7 +73,7 @@ int32_t WaveOsc_next(WaveOsc *self) {
     if (self->phase[0] >= self->limit[0]) {
       self->phase[0] = 0;
     }
-    val = sinewave_sample(self->wave[0], self->phase[0]) >> self->quiet[0];
+    val = wavetable_sample(self->wave[0], self->phase[0]) >> self->quiet[0];
     self->phase[0]++;
   }
   return val;
