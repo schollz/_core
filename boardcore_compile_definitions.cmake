3604a48b4f285e32ea759333454a543d48bcc4a4
Zack Scholl <zack.scholl@gmail.com>
2024-01-13 10:21:12 -0800

    feat: add different compile definitions

---
+++ boardcore_compile_definitions.cmake
@@ -0,0 +1,28 @@
+target_compile_definitions(${PROJECT_NAME} PRIVATE
+    # hardware specific
+    USE_AUDIO_I2S=1
+    SDCARD_USE_CD=false
+    SDCARD_CD_GPIO=21 # not used...
+
+    # utilize core1 for audio to avoid dropouts
+    CORE1_PROCESS_I2S_CALLBACK=1 
+    DO_OVERCLOCK=1
+
+    # debug printing
+    # PRINT_AUDIO_USAGE=1
+    # PRINT_AUDIO_OVERLOADS=1
+    # PRINT_AUDIO_CPU_USAGE=1
+    # PRINT_MEMORY_USAGE=1
+    # PRINT_SDCARD_TIMING=1
+
+    # turn off gpio for leds
+    LEDS_NO_GPIO=1
+
+    # file variations
+    FILE_VARIATIONS=2
+
+    # basics 
+    # INCLUDE_KEYBOARD=1
+    # INCLUDE_RGBLED=1
+    # INCLUDE_SINEBASS=1
+)
---
+++ ectocore_compile_definitions.cmake
@@ -0,0 +1,35 @@
+target_compile_definitions(${PROJECT_NAME} PRIVATE
+    # hardware specific
+    USE_AUDIO_I2S=1
+    SDCARD_USE_CD=false
+    SDCARD_CD_GPIO=21 # not used...
+
+    # # ecto
+    SDCARD_CMD_GPIO=25
+    SDCARD_D0_GPIO=26 
+    AUDIO_CLK_GPIO=21 # LCK=17
+    AUDIO_DIN_GPIO=23
+    INCLUDE_ECTOCORE=1
+
+    # utilize core1 for audio to avoid dropouts
+    CORE1_PROCESS_I2S_CALLBACK=1 
+    DO_OVERCLOCK=1
+
+    # debug printing
+    # PRINT_AUDIO_USAGE=1
+    # PRINT_AUDIO_OVERLOADS=1
+    # PRINT_AUDIO_CPU_USAGE=1
+    # PRINT_MEMORY_USAGE=1
+    # PRINT_SDCARD_TIMING=1
+
+    # turn off gpio for leds
+    LEDS_NO_GPIO=1
+
+    # file variations
+    FILE_VARIATIONS=2
+
+    # basics 
+    # INCLUDE_KEYBOARD=1
+    # INCLUDE_RGBLED=1
+    # INCLUDE_SINEBASS=1
+)
