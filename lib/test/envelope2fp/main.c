4312d16136bc92d3e9777cf4e46bd19a8fee08e3
Zack Scholl <zack.scholl@gmail.com>
2024-01-22 15:46:54 -0800

    simple lfo
    Former-commit-id: 5cffa83aa69c657c9afb0be7e1e20c83062db2d7

--- lib/test/envelope2fp/main.c
+++ lib/test/envelope2fp/main.c
@@ -34,17 +34,30 @@ int main() {
   // Create a Envelope2 instance
   float sampleRate = 441;
 
-  Envelope2 *envelope2 = Envelope2_create(sampleRate, 3, 20, 2);
-  for (int i = 0; i < 441 * 2; i++) {
-    float value = Envelope2_update(envelope2);
-    printf("%2.3f\n", value);
-  }
-  envelope2 = Envelope2_create(sampleRate, 20, 0.01, 1);
+  Envelope2 *envelope2;
+
+  // envelope2= Envelope2_create(sampleRate, 3, 20, 2);
+  // for (int i = 0; i < 441 * 2; i++) {
+  //   float value = Envelope2_update(envelope2);
+  //   printf("%2.3f\n", value);
+  // }
+  // envelope2 = Envelope2_create(sampleRate, 20, 0.01, 1);
+  // for (int i = 0; i < 441; i++) {
+  //   float value = Envelope2_update(envelope2);
+  //   printf("%2.3f\n", value);
+  // }
+  // Envelope2_destroy(envelope2);
+
+  envelope2 = Envelope2_create(sampleRate, 1, -1, 1);
   for (int i = 0; i < 441; i++) {
     float value = Envelope2_update(envelope2);
     printf("%2.3f\n", value);
   }
-
+  Envelope2_reset(envelope2, sampleRate, -1, 1, 1);
+  for (int i = 0; i < 441; i++) {
+    float value = Envelope2_update(envelope2);
+    printf("%2.3f\n", value);
+  }
   Envelope2_destroy(envelope2);
 
   return 0;
