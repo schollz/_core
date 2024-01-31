ebcb01033b1cd8a7ac092251307f6c5bb2ff4ffe
Zack Scholl <zack.scholl@gmail.com>
2024-01-31 13:07:57 -0800

    bump version
    Former-commit-id: d061c6c5f4b843687e378e9e7e5bf94b0c538b13

--- lib/zeptocore.h
+++ lib/zeptocore.h
@@ -137,7 +137,7 @@ void input_handling() {
   uint8_t debounce_beat_repeat = 0;
 
   // debug test
-  printStringWithDelay("v1.0.0");
+  printStringWithDelay("v1.0.1");
 
   while (1) {
     // TODO: check timing of this?
