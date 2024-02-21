f69ccdf7bf88a143f41da0a56b326837d020bcf4
Zack <zack.scholl@gmail.com>
2024-02-21 11:57:18 -0800

    bump version

--- core/src/server/server.go
+++ core/src/server/server.go
@@ -234,7 +234,7 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 				IsBuy:          r.URL.Path[1:] == "buy",
 				IsMain:         r.URL.Path == "/",
 				IsZeptocore:    r.URL.Path == "/zeptocore",
-				VersionCurrent: "v1.3.1",
+				VersionCurrent: "v1.4.0",
 				GenURL1:        codename.Generate(rng, 0),
 				GenURL2:        names.Random(),
 			}
