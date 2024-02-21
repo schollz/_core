c5dd73df1bfe9a83c3640234d100ace4636d0eb6
Zack <zack.scholl@gmail.com>
2024-02-21 11:25:29 -0800

    refactor auto load
    Former-commit-id: 0eb44afda23296a6b113cf7111d3d1c60dae9652

--- lib/boardcore.h
+++ lib/boardcore.h
@@ -22,7 +22,7 @@ void input_handling() {
     if (c >= 0) {
       printf("Got character %d\n", c);
       if (c == 118) {
-        printf("version=v1.3.0\n");
+        printf("version=v1.3.1\n");
       }
 
       for (int i = 0; i < 16; i++) {
