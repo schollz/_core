ec5a0211f20e713b9b271d344972d4de2affcd57
Zack <zack.scholl@gmail.com>
2024-02-27 16:27:53 -0800

    bump version

--- lib/ectocore.h
+++ lib/ectocore.h
@@ -217,7 +217,7 @@ void input_handling() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v1.6.2\n");
+        printf("version=v1.6.3\n");
       }
     }
 
