2146be25e686a5d767d7106fcfbff81a73f8aceb
Zack <zack.scholl@gmail.com>
2024-03-01 13:05:29 -0800

    bump version

--- lib/zeptocore.h
+++ lib/zeptocore.h
@@ -141,10 +141,10 @@ void input_handling() {
   uint8_t debounce_beat_repeat = 0;
 
   // debug test
-  printStringWithDelay("zv2.0.0");
+  printStringWithDelay("zv2.0.1");
 
   // print to screen
-  printf("version=v2.0.0\n");
+  printf("version=v2.0.1\n");
 
   // initialize the resonsant filter
   global_filter_index = 12;
@@ -168,7 +168,7 @@ void input_handling() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v2.0.0\n");
+        printf("version=v2.0.1\n");
       }
     }
     // TODO: check timing of this?
