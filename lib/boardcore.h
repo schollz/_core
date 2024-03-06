fd8f42952bc3a173e1cb5fdf9ebb579886c081d3
Zack <zack.scholl@gmail.com>
2024-03-06 14:16:58 -0800

    bump version

--- lib/boardcore.h
+++ lib/boardcore.h
@@ -22,7 +22,7 @@ void input_handling() {
     if (c >= 0) {
       printf("Got character %d\n", c);
       if (c == 118) {
-        printf("version=v2.0.5\n");
+        printf("version=v2.0.6\n");
       }
 
       for (int i = 0; i < 16; i++) {
