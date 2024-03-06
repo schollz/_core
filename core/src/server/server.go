f0cf77736ff4b588a71711903f15862f306e3000
Zack <zack.scholl@gmail.com>
2024-03-06 14:16:58 -0800

    bump version
    Former-commit-id: fd8f42952bc3a173e1cb5fdf9ebb579886c081d3

--- core/src/server/server.go
+++ core/src/server/server.go
@@ -259,7 +259,7 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 				IsBuy:          r.URL.Path[1:] == "buy",
 				IsMain:         r.URL.Path == "/",
 				IsZeptocore:    r.URL.Path == "/zeptocore",
-				VersionCurrent: "v2.0.5",
+				VersionCurrent: "v2.0.6",
 				GenURL1:        codename.Generate(rng, 0),
 				GenURL2:        names.Random(),
 			}
