9d022baa47d5440fa90f2e2aa8fd2126c6529aaa
Zack <zack.scholl@gmail.com>
2024-03-06 15:47:27 -0800

    add flashing scripts
    Former-commit-id: df84846ee07952a9ba9b17dd85b3e688de26fd61

---
+++ dev/flash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true
+do
+if mount | grep RPI-RP2 > /dev/null; then
+	sleep 1
+	echo "uploading..."
+	time pv -batep zeptocore_v2.0.6.uf2  > /media/zns/RPI-RP2/zeptocore.uf2
+    echo "uploaded!"
+fi
+sleep 0.5
+done
