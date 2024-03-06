b31cdeffc5e13170c7fe98d8d3957c310ae67863
Zack <zack.scholl@gmail.com>
2024-03-05 16:46:11 -0800

    bump version
    Former-commit-id: e02ca8e66d88c779bb8311d419aa5641c35bf9ef

--- lib/boardcore.h
+++ lib/boardcore.h
@@ -22,7 +22,7 @@ void input_handling() {
     if (c >= 0) {
       printf("Got character %d\n", c);
       if (c == 118) {
-        printf("version=v2.0.3\n");
+        printf("version=v2.0.4\n");
       }
 
       for (int i = 0; i < 16; i++) {
