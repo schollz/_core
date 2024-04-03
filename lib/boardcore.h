ff533c81d6776c7df8c260065273e83202f0f5ba
Zack <zack.scholl@gmail.com>
2024-04-03 09:15:43 -0700

    fix boardcore timestretch
    Former-commit-id: 84d4dacc0414106a1c1da9d2abb84f519294a077

--- lib/boardcore.h
+++ lib/boardcore.h
@@ -21,6 +21,41 @@ void input_handling() {
       MessageSync_print(messagesync);
       MessageSync_clear(messagesync);
     }
-    sleep_ms(1);
+
+    // load the new sample if variation changed
+    if (sel_variation_next != sel_variation) {
+      if (!audio_callback_in_mute) {
+        while (!sync_using_sdcard) {
+          sleep_us(250);
+        }
+        while (sync_using_sdcard) {
+          sleep_us(250);
+        }
+      }
+      sync_using_sdcard = true;
+      // measure the time it takes
+      uint32_t time_start = time_us_32();
+      FRESULT fr = f_close(&fil_current);
+      if (fr != FR_OK) {
+        debugf("[zeptocore] f_close error: %s\n", FRESULT_str(fr));
+      }
+      sprintf(fil_current_name, "bank%d/%d.%d.wav", sel_bank_cur,
+              sel_sample_cur, sel_variation_next);
+      fr = f_open(&fil_current, fil_current_name, FA_READ);
+      if (fr != FR_OK) {
+        debugf("[zeptocore] f_close error: %s\n", FRESULT_str(fr));
+      }
+
+      // TODO: fix this
+      // if sel_variation_next == 0
+      phases[0] = round(
+          ((float)phases[0] * (float)sel_variation_scale[sel_variation_next]) /
+          (float)sel_variation_scale[sel_variation]);
+
+      sel_variation = sel_variation_next;
+      sync_using_sdcard = false;
+      printf("[zeptocore] loading new sample variation took %d us\n",
+             time_us_32() - time_start);
+    }
   }
 }
