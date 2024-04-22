27efa566999d04ea54c605b90f73a53ea9ca120d
Zack Scholl <zack.scholl@gmail.com>
2024-04-22 14:05:22 -0700

    add initial splash

---
+++ docs/themes/zeptocore/static/static/splash.js
@@ -0,0 +1,37 @@
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function () {
+    modal2.style.opacity = 0;
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal2) {
+        modal2.style.opacity = 0;
+    }
+}
+
+// When the user clicks any link in the modal, close it
+var links = document.querySelectorAll('#myModal2 a');
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function () {
+        modal2.style.opacity = 0;
+    });
+}
+
+// // if splash=1 cookie not found
+// if (localStorage.getItem('splash') !== '1') {
+//     // show modal after 1 second
+//     setTimeout(function () {
+//         // fade in the modal
+//         modal2.style.opacity = 1;
+//         localStorage.setItem('splash', '1');
+//     }, 100);
+// }
