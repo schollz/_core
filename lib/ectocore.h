fcec6e4f2121138eca702fda9fe73f4577984833
Zack Scholl <zack.scholl@gmail.com>
2024-05-19 15:17:20 -0700

    v2.7.2

--- lib/ectocore.h
+++ lib/ectocore.h
@@ -563,7 +563,7 @@ void input_handling() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v2.7.1\n");
+        printf("version=v2.7.2\n");
       }
     }
 
--- lib/zeptocore.h
+++ lib/zeptocore.h
@@ -133,10 +133,10 @@ void input_handling() {
   uint8_t debounce_beat_repeat = 0;
 
   // debug test
-  printStringWithDelay("zv2.7.1");
+  printStringWithDelay("zv2.7.2");
 
   // print to screen
-  printf("version=v2.7.1\n");
+  printf("version=v2.7.2\n");
 
   // initialize the resonsant filter
   global_filter_index = 12;
@@ -215,7 +215,7 @@ void input_handling() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v2.7.1\n");
+        printf("version=v2.7.2\n");
       }
     }
 
