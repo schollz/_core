8e602f8dd49f37f8272edac69a086119a9371f5f
Zack Scholl <zack.scholl@gmail.com>
2024-06-17 15:05:16 -0700

    v3.0.0

--- lib/ectocore.h
+++ lib/ectocore.h
@@ -686,7 +686,7 @@ void input_handling() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v2.9.2\n");
+        printf("version=v3.0.0\n");
       }
     }
 
