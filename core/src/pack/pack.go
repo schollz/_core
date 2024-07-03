310047f31e62b32fff81c093d1af9e836a0d84fe
Zack <zack.scholl@gmail.com>
2024-07-02 17:07:58 -0700

    core: download only the settings Fixes #542

--- core/src/pack/pack.go
+++ core/src/pack/pack.go
@@ -30,7 +30,7 @@ type Data struct {
 	} `json:"banks"`
 }
 
-func Zip(pathToStorage string, payload []byte) (zipFilename string, err error) {
+func Zip(pathToStorage string, payload []byte, settingsOnly bool) (zipFilename string, err error) {
 	zipStorage := path.Join(pathToStorage, "zips")
 	os.MkdirAll(zipStorage, 0777)
 
@@ -65,26 +65,28 @@ func Zip(pathToStorage string, payload []byte) (zipFilename string, err error) {
 		return
 	}
 
-	// create the bank folders
-	for i, bank := range data.Banks {
-		log.Tracef("bank %d has %d files", i, len(bank.Files))
-		if len(bank.Files) == 0 {
-			continue
-		}
-		// process each file according to parameters
-		for _, file := range bank.Files {
-			log.Tracef("bank %d: %s", i, file)
-			// get the file information
-			var f zeptocore.File
-			f, err = zeptocore.Get(path.Join(pathToStorage, file, file))
-			if err != nil {
-				log.Error(err)
-				return
+	if !settingsOnly {
+		// create the bank folders
+		for i, bank := range data.Banks {
+			log.Tracef("bank %d has %d files", i, len(bank.Files))
+			if len(bank.Files) == 0 {
+				continue
 			}
-			f.SetOversampling(oversampling)
+			// process each file according to parameters
+			for _, file := range bank.Files {
+				log.Tracef("bank %d: %s", i, file)
+				// get the file information
+				var f zeptocore.File
+				f, err = zeptocore.Get(path.Join(pathToStorage, file, file))
+				if err != nil {
+					log.Error(err)
+					return
+				}
+				f.SetOversampling(oversampling)
+			}
 		}
+		time.Sleep(200 * time.Millisecond)
 	}
-	time.Sleep(200 * time.Millisecond)
 
 	// wait until all the files are processed
 	for i := 0; i < 3000; i++ {
@@ -105,81 +107,89 @@ func Zip(pathToStorage string, payload []byte) (zipFilename string, err error) {
 		log.Error(err)
 		return
 	}
+	settingsFolder := path.Join(mainFolder, "settings")
+	err = os.MkdirAll(settingsFolder, 0777)
+	if err != nil {
+		log.Error(err)
+		return
+	}
 	if data.Resampling == "linear" {
-		os.Create(path.Join(mainFolder, "resampling_quadratic-off"))
+		os.Create(path.Join(settingsFolder, "resampling_quadratic-off"))
 	} else {
-		os.Create(path.Join(mainFolder, "resampling_quadratic-on"))
+		os.Create(path.Join(settingsFolder, "resampling_quadratic-on"))
 	}
 	if data.SettingsClockStop {
-		os.Create(path.Join(mainFolder, "clock_stop_sync-on"))
+		os.Create(path.Join(settingsFolder, "clock_stop_sync-on"))
 	} else {
-		os.Create(path.Join(mainFolder, "clock_stop_sync-off"))
+		os.Create(path.Join(settingsFolder, "clock_stop_sync-off"))
 	}
 	if data.SettingsClockOutput {
-		os.Create(path.Join(mainFolder, "clock_output_trig-on"))
+		os.Create(path.Join(settingsFolder, "clock_output_trig-on"))
 	} else {
-		os.Create(path.Join(mainFolder, "clock_output_trig-off"))
+		os.Create(path.Join(settingsFolder, "clock_output_trig-off"))
 	}
 
 	if data.SettingsClockBehaviorSync {
-		os.Create(path.Join(mainFolder, "clock_behavior_sync_slice-on"))
+		os.Create(path.Join(settingsFolder, "clock_behavior_sync_slice-on"))
 	} else {
-		os.Create(path.Join(mainFolder, "clock_behavior_sync_slice-off"))
+		os.Create(path.Join(settingsFolder, "clock_behavior_sync_slice-off"))
 	}
 
 	// knob x
 	if data.SettingsKnobXSample {
-		os.Create(path.Join(mainFolder, "knobx_select_sample-on"))
+		os.Create(path.Join(settingsFolder, "knobx_select_sample-on"))
 	} else {
-		os.Create(path.Join(mainFolder, "knobx_select_sample-off"))
+		os.Create(path.Join(settingsFolder, "knobx_select_sample-off"))
 	}
 	// brightness
-	os.Create(path.Join(mainFolder, fmt.Sprintf("brightness-%d", data.SettingsBrightness)))
+	os.Create(path.Join(settingsFolder, fmt.Sprintf("brightness-%d", data.SettingsBrightness)))
 
 	// grimoire effects
 	for i, effect := range data.SettingsGrimoireEffects {
-		os.MkdirAll(path.Join(mainFolder, "grimoire", fmt.Sprintf("rune%d", i+1)), 0777)
+		os.MkdirAll(path.Join(settingsFolder, "grimoire", fmt.Sprintf("rune%d", i+1)), 0777)
 		for j, on := range effect {
 			if on {
-				os.Create(path.Join(mainFolder, "grimoire", fmt.Sprintf("rune%d", i+1), fmt.Sprintf("effect%d-on", j+1)))
+				os.Create(path.Join(settingsFolder, "grimoire", fmt.Sprintf("rune%d", i+1), fmt.Sprintf("effect%d-on", j+1)))
 			} else {
-				os.Create(path.Join(mainFolder, "grimoire", fmt.Sprintf("rune%d", i+1), fmt.Sprintf("effect%d-off", j+1)))
+				os.Create(path.Join(settingsFolder, "grimoire", fmt.Sprintf("rune%d", i+1), fmt.Sprintf("effect%d-off", j+1)))
 			}
 		}
 	}
 
-	// copy files
-	for i, bank := range data.Banks {
-		if len(bank.Files) == 0 {
-			continue
-		}
-		log.Tracef("bank %d has %d files", i, len(bank.Files))
-		bankFolder := path.Join(zipStorage, zipFilename, fmt.Sprintf("bank%d", i))
-		err = os.MkdirAll(bankFolder, 0777)
-		if err != nil {
-			return
-		}
-		// go through each file and copy it into the bank
-		for filei, file := range bank.Files {
-			log.Tracef("bank %d: %s", i, file)
-			filenameWithoutExtension := file[:len(file)-len(path.Ext(file))]
-			for i := 0; i < 100; i++ {
-				oldFname := path.Join(pathToStorage, file, fmt.Sprintf("%s.%d.wav", filenameWithoutExtension, i))
-				newFname := path.Join(bankFolder, fmt.Sprintf("%d.%d.wav", filei, i))
-				if _, err := os.Stat(oldFname); os.IsNotExist(err) {
-					break
-				}
-				// copy wav file
-				err = utils.CopyFile(oldFname, newFname)
-				if err != nil {
-					log.Error(err)
-					return
-				}
-				// copy info file
-				err = utils.CopyFile(oldFname+".info", newFname+".info")
-				if err != nil {
-					log.Error(err)
-					return
+	if !settingsOnly {
+		// copy files
+		for i, bank := range data.Banks {
+			if len(bank.Files) == 0 {
+				continue
+			}
+			log.Tracef("bank %d has %d files", i, len(bank.Files))
+			bankFolder := path.Join(zipStorage, zipFilename, fmt.Sprintf("bank%d", i))
+			err = os.MkdirAll(bankFolder, 0777)
+			if err != nil {
+				return
+			}
+			// go through each file and copy it into the bank
+			for filei, file := range bank.Files {
+				log.Tracef("bank %d: %s", i, file)
+				filenameWithoutExtension := file[:len(file)-len(path.Ext(file))]
+				for i := 0; i < 100; i++ {
+					oldFname := path.Join(pathToStorage, file, fmt.Sprintf("%s.%d.wav", filenameWithoutExtension, i))
+					newFname := path.Join(bankFolder, fmt.Sprintf("%d.%d.wav", filei, i))
+					if _, err := os.Stat(oldFname); os.IsNotExist(err) {
+						break
+					}
+					// copy wav file
+					err = utils.CopyFile(oldFname, newFname)
+					if err != nil {
+						log.Error(err)
+						return
+					}
+					// copy info file
+					err = utils.CopyFile(oldFname+".info", newFname+".info")
+					if err != nil {
+						log.Error(err)
+						return
+					}
 				}
 			}
 		}
