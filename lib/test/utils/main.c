593e737f4f3ebf19c441861536782fdeba63d019
Zack Scholl <zack.scholl@gmail.com>
2024-08-28 10:40:00 -0700

    show bank in sections of led

--- lib/test/utils/main.c
+++ lib/test/utils/main.c
@@ -6,7 +6,7 @@
 
 void print_rhythm(bool *rhythm, int n) {
   for (int i = 0; i < n; i++) {
-    printf("%c ", rhythm[i] ? 'X' : '.');
+    printf("%d ", rhythm[i]);
   }
   printf("\n");
 }
@@ -23,11 +23,42 @@ int main() {
   printf("map(%d, 0, 30, 0, 888888) = %d\n", x, linlin(x, 0, 30, 0, 888888));
 
   int n = 16;
-  int k = 8;
+  int k = 7;
   bool rhythm[n];
 
-  generate_euclidean_rhythm(n, k, (n / k) / 2 + 1, rhythm);
+  generate_euclidean_rhythm(n, k, 0, rhythm);
+  uint8_t arr[n];
+
   print_rhythm(rhythm, n);
+
+  uint8_t total = 0;
+  for (int i = 0; i < n; i++) {
+    arr[i] = rhythm[i];
+    total += arr[i];
+  }
+  if (total == 0) {
+    return;
+  }
+
+  // rotate until i=0 has a 1 in it
+  while (arr[0] == 0) {
+    bool tmp = arr[n - 1];
+    for (int j = n - 1; j > 0; j--) {
+      arr[j] = arr[j - 1];
+    }
+    arr[0] = tmp;
+  }
+
+  // cumulative sum of arr
+  arr[0] = 0;
+  for (int i = 1; i < n; i++) {
+    arr[i] += arr[i - 1];
+  }
+
+  // print out arr
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
 
   return 0;
 }
