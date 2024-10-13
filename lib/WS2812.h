ec26566cd7df6665cf418bef1f187ef7e9c59513
Zack Scholl <zack.scholl@gmail.com>
2024-10-13 00:51:06 -0700

    add color defs

--- lib/WS2812.h
+++ lib/WS2812.h
@@ -27,6 +27,36 @@
 
 #include "WS2812.pio.h"
 
+typedef struct
+{
+  int red;
+  int green;
+  int blue;
+} RGBColor;
+
+RGBColor colors_defs[] = {
+    {255, 0, 0},   // red
+    {254, 255, 0}, // orange/yellow
+    {0, 255, 0},   // green
+    {0, 57, 255},  // cyan
+    {0, 0, 255},   // blue
+    {41, 0, 255},  // magenta
+    {0, 0, 0},     // blank
+
+};
+
+// create enum for colors
+typedef enum
+{
+  RED = 0,
+  ORANGE = 1,
+  GREEN = 2,
+  CYAN = 3,
+  BLUE = 4,
+  MAGENTA = 5,
+  BLANK = 6,
+} Color;
+
 #define NUM_LEDS 18
 
 const uint8_t ws2812_brightness_values[16] = {
@@ -35,7 +65,8 @@ const uint8_t ws2812_brightness_values[16] = {
     // 140, 164, 192, 222, 255,
 };
 
-typedef struct WS2812 {
+typedef struct WS2812
+{
   uint pin;
   PIO pio;
   uint sm;
@@ -44,7 +75,8 @@ typedef struct WS2812 {
   uint8_t brightness;
 } WS2812;
 
-WS2812 *WS2812_new(uint pin, PIO pio, uint sm) {
+WS2812 *WS2812_new(uint pin, PIO pio, uint sm)
+{
   WS2812 *ws;
   ws = malloc(sizeof(WS2812));
   ws->pin = pin;
@@ -62,22 +94,33 @@ WS2812 *WS2812_new(uint pin, PIO pio, uint sm) {
   return ws;
 }
 
-void WS2812_set_brightness(WS2812 *ws, uint8_t brightness) {
+void WS2812_set_brightness(WS2812 *ws, uint8_t brightness)
+{
   // brightness between 0 and 100
-  if (brightness > 100) {
+  if (brightness > 100)
+  {
     brightness = 100;
   }
   ws->brightness = ws2812_brightness_values[brightness * 16 / 100];
-  if (brightness > 0 && ws->brightness == 0) {
+  if (brightness > 0 && ws->brightness == 0)
+  {
     ws->brightness = 1;
   }
   return;
 }
 
+void WS2812_fill_color(WS2812 *ws, int index, Color color)
+{
+  WS2812_fill(ws, index, colors_defs[color].red, colors_defs[color].green,
+              colors_defs[color].blue);
+}
+
 void WS2812_fill(WS2812 *ws, int index, uint8_t red, uint8_t green,
-                 uint8_t blue) {
-  if (index < 0 || index >= NUM_LEDS) {
-    return;  // Safety check to avoid overflow
+                 uint8_t blue)
+{
+  if (index < 0 || index >= NUM_LEDS)
+  {
+    return; // Safety check to avoid overflow
   }
   // scale by brightness level
   red = (red * ws->brightness) / 255;
@@ -86,29 +129,33 @@ void WS2812_fill(WS2812 *ws, int index, uint8_t red, uint8_t green,
   uint32_t rgbw =
       (uint32_t)(blue) << 16 | (uint32_t)(green) << 8 | (uint32_t)(red);
   uint32_t result = 0;
-  for (uint b = 0; b < 4; b++) {
-    switch (ws->bytes[b]) {
-      case 1:
-        result |= (rgbw & 0xFF);
-        break;
-      case 2:
-        result |= (rgbw & 0xFF00) >> 8;
-        break;
-      case 3:
-        result |= (rgbw & 0xFF0000) >> 16;
-        break;
-      case 4:
-        result |= (rgbw & 0xFF000000) >> 24;
-        break;
+  for (uint b = 0; b < 4; b++)
+  {
+    switch (ws->bytes[b])
+    {
+    case 1:
+      result |= (rgbw & 0xFF);
+      break;
+    case 2:
+      result |= (rgbw & 0xFF00) >> 8;
+      break;
+    case 3:
+      result |= (rgbw & 0xFF0000) >> 16;
+      break;
+    case 4:
+      result |= (rgbw & 0xFF000000) >> 24;
+      break;
     }
     result <<= 8;
   }
-  ws->data[index] = result;  // Store data for the specified LED
+  ws->data[index] = result; // Store data for the specified LED
   return;
 }
 
-void WS2812_show(WS2812 *ws) {
-  for (int i = 0; i < NUM_LEDS; i++) {
+void WS2812_show(WS2812 *ws)
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
     pio_sm_put_blocking(ws->pio, ws->sm, ws->data[i]);
   }
   return;
