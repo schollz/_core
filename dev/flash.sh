a8fc55c467c5d35f249f82ea82b05219a4dd36f3
Zack <zack.scholl@gmail.com>
2024-11-17 07:06:14 -0800

    v6.2.4

--- dev/flash.sh
+++ dev/flash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-curl -L https://github.com/schollz/_core/releases/download/v6.2.3/zeptocore_v6.2.3.uf2 > zeptocore_v6.2.3.uf2
+curl -L https://github.com/schollz/_core/releases/download/v6.2.4/zeptocore_v6.2.4.uf2 > zeptocore_v6.2.4.uf2
 while true
 do
 if mount | grep RPI-RP2 > /dev/null; then
 	sleep 1
 	echo "uploading..."
-	time pv -batep zeptocore_v6.2.3.uf2  > /media/zns/RPI-RP2/zeptocore.uf2
+	time pv -batep zeptocore_v6.2.4.uf2  > /media/zns/RPI-RP2/zeptocore.uf2
     echo "uploaded!"
 fi
 sleep 0.5
