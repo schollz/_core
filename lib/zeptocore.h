6e7115e39e89bf5645dd36d94252fe219b9f9a7e
Zack Scholl <zack.scholl@gmail.com>
2024-11-23 17:59:46 -0800

    v6.2.7

--- lib/zeptocore.h
+++ lib/zeptocore.h
@@ -165,10 +165,10 @@ void input_handling() {
   uint8_t sample_selection_index = 0;
 
   // debug test
-  printStringWithDelay("zv6.2.6");
+  printStringWithDelay("zv6.2.7");
 
   // print to screen
-  printf("version=v6.2.6\n");
+  printf("version=v6.2.7\n");
 
   // initialize the resonsant filter
   global_filter_index = 12;
@@ -266,7 +266,7 @@ void input_handling() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v6.2.6\n");
+        printf("version=v6.2.7\n");
       }
     }
 
