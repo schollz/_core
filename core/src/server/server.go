ed91767e84e5995a6e98d41f7c449d2233adca57
Zack Scholl <zack.scholl@gmail.com>
2024-12-15 07:55:54 -0800

    v6.2.12

--- core/src/server/server.go
+++ core/src/server/server.go
@@ -209,7 +209,7 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-		b, _ := json.Marshal(map[string]string{"version": "v6.2.10"})
+		b, _ := json.Marshal(map[string]string{"version": "v6.2.12"})
 		w.Write(b)
 		return nil
 	} else if r.URL.Path == "/ws" {
@@ -301,7 +301,7 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 				GenURL2        string
 			}{
 				IsMain:         r.URL.Path == "/",
-				VersionCurrent: "v6.2.10",
+				VersionCurrent: "v6.2.12",
 				GenURL1:        codename.Generate(rng, 0),
 				GenURL2:        names.Random(),
 				IsEctocore:     isEctocore,
