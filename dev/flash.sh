6fe5e9462ea1fd127b8c0579f66f6d71f22d9989
Zack Scholl <zack.scholl@gmail.com>
2025-02-05 06:57:58 -0800

    v6.2.16

--- dev/flash.sh
+++ dev/flash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-curl -L https://github.com/schollz/_core/releases/download/v6.2.15/zeptocore_v6.2.15.uf2 > zeptocore_v6.2.15.uf2
+curl -L https://github.com/schollz/_core/releases/download/v6.2.16/zeptocore_v6.2.16.uf2 > zeptocore_v6.2.16.uf2
 while true
 do
 if mount | grep RPI-RP2 > /dev/null; then
 	sleep 1
 	echo "uploading..."
-	time pv -batep zeptocore_v6.2.15.uf2  > /media/zns/RPI-RP2/zeptocore.uf2
+	time pv -batep zeptocore_v6.2.16.uf2  > /media/zns/RPI-RP2/zeptocore.uf2
     echo "uploaded!"
 fi
 sleep 0.5
--- lib/ectocore.h
+++ lib/ectocore.h
@@ -914,7 +914,7 @@ void __not_in_flash_func(input_handling)() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v6.2.15\n");
+        printf("version=v6.2.16\n");
       }
     }
 
