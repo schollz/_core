f574b63305af4eb797980ac255398903ed20f86c
Zack <zack.scholl@gmail.com>
2025-10-25 08:14:23 -0700

    bump: 6.4.4
    Former-commit-id: 819fe7229186e77e7cf6449b5773e51f75302daf

--- lib/zeptocore.h
+++ lib/zeptocore.h
@@ -141,10 +141,10 @@ void __not_in_flash_func(input_handling)() {
   uint8_t sample_selection_index = 0;
 
   // debug test
-  printStringWithDelay("zv6.4.3");
+  printStringWithDelay("zv6.4.4");
 
   // print to screen
-  printf("version=v6.4.3\n");
+  printf("version=v6.4.4\n");
 
   // initialize the resonsant filter
   global_filter_index = 12;
@@ -240,7 +240,7 @@ void __not_in_flash_func(input_handling)() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v6.4.3\n");
+        printf("version=v6.4.4\n");
       }
     }
 
