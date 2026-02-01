c97aea1d66cf9ab320ba9897bf5ed78948095306
Zack <zack.scholl@gmail.com>
2026-02-01 09:28:47 -0800

    feat: backporting ezeptocore firmware to ectocore (#806)
    * feat: backporting ezeptocore firmware to ectocore
    
    * fix: versioning
    
    * add about
    
    * update ABOUT
    
    * fix: update to latest version
    
    Former-commit-id: f2a17548194228ce7735a4dc8da68381b29a4ad1

--- lib/ectocore.h
+++ lib/ectocore.h
@@ -1034,7 +1034,7 @@ void __not_in_flash_func(input_handling)() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v7.1.0\n");
+        printf("version=v7.1.1\n");
       }
     }
 
