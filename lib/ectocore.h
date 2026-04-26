9d1c3cabd58045faab885507c48b5b3c6c7cabfe
Zack Scholl <zack.scholl@gmail.com>
2026-04-26 12:54:01 -0700

    fix: bump version

--- lib/ectocore.h
+++ lib/ectocore.h
@@ -1088,7 +1088,7 @@ void __not_in_flash_func(input_handling)() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v7.1.4\n");
+        printf("version=v7.1.5\n");
       }
     }
 
