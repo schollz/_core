aca9b24aa912ffdc2bb32ba3aaaff8d0ffb34de1
Zack Scholl <zack.scholl@gmail.com>
2026-06-23 09:39:44 -0700

    v7.3.0

--- lib/ectocore.h
+++ lib/ectocore.h
@@ -1103,7 +1103,7 @@ void __not_in_flash_func(input_handling)() {
     int char_input = getchar_timeout_us(10);
     if (char_input >= 0) {
       if (char_input == 118) {
-        printf("version=v7.1.8\n");
+        printf("version=v7.3.0\n");
       }
     }
 
