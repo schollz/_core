dc38b21f8556815ca3985b0e2199f2f23de93596
Zack Scholl <zack.scholl@gmail.com>
2026-07-25 09:29:50 +0000

    fix: server
    Former-commit-id: 6867236230d86c67b228518e69c4e318196f5232

--- core/src/server/server.go
+++ core/src/server/server.go
@@ -195,7 +195,7 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-		b, _ := json.Marshal(map[string]string{"version": "v7.1.3"})
+		b, _ := json.Marshal(map[string]string{"version": "v7.3.0"})
 		w.Write(b)
 		return nil
 	} else if r.URL.Path == "/ws" {
@@ -293,7 +293,7 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 				GenURL2        string
 			}{
 				IsMain:         r.URL.Path == "/",
-				VersionCurrent: "v7.1.3",
+				VersionCurrent: "v7.3.0",
 				LatestVersion:  latestTag,
 				GenURL1:        codename.Generate(rng, 0),
 				GenURL2:        names.Random(),
